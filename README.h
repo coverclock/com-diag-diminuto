016fcdf9a9cbee6197abd20e6a697af2b4fbd4ee
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2008-08-25 23:20:00 +0000

    Updated licensing to "modified LGPL".
    git-svn-id: svn://graphite/diminuto/trunk/Diminuto@13 5dfbf39a-1452-0410-8a97-9e58fedde8e3

---
+++ README.h
@@ -0,0 +1,77 @@
+/* vim: set ts=4 expandtab shiftwidth=4: */
+#ifndef _H_DIMINUTO_README_
+#define _H_DIMINUTO_README_
+
+/*******************************************************************************
+
+    Copyright 2008 by the Digital Aggregates Corp., Arvada CO 80001-0587 USA.
+
+    ----------------------------------------------------------------------
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    As a special exception, if other files instantiate templates or
+    use macros or inline functions from this file, or you compile
+    this file and link it with other works to produce a work based on
+    this file, this file does not by itself cause the resulting work
+    to be covered by the GNU Lesser General Public License. However
+    the source code for this file must still be made available in
+    accordance with the GNU Lesser General Public License.
+
+    This exception does not invalidate any other reasons why a work
+    based on this file might be covered by the GNU Lesser General
+    Public License.
+
+    Alternative commercial licensing terms are available from the copyright
+    holder. Contact Digital Aggregates Corporation for more information.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, contact
+
+        Free Software Foundation, Inc.
+        59 Temple Place, Suite 330
+        Boston MA 02111-1307 USA
+        http://www.gnu.org/copyleft/lesser.txt
+
+    ----------------------------------------------------------------------
+
+    ABSTRACT
+
+    This file is part of the Digital Aggregates Corporation Diminuto
+    library. Diminuto is an open source library of systems programming
+    functions written in C to simplify the writing of device drivers and
+    daemons for the Linux operating system.
+
+    Contact
+
+        Chip Overclock
+        mailto:coverclock@diag.com
+        Digital Aggregates Corporation
+        Arvada CO 80001-0587 USA
+
+    ----------------------------------------------------------------------
+
+    INSTALLATION
+
+    Here is the sequence of commands to build Diminuto:
+
+    vi Makefile                 # establish your configuration
+    make all                  	# make all
+
+*******************************************************************************/
+
+/**
+ *  @file
+ *
+ *  This is the README for this project.
+ */
+
+#endif
