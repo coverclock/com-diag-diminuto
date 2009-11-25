aa916c7af33a0c4f33869440e742ea4e2208a399
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2009-11-25 23:22:19 +0000

    Added ipcalc open source implementation.
    git-svn-id: svn://graphite/diminuto/trunk/Diminuto@42 5dfbf39a-1452-0410-8a97-9e58fedde8e3

---
+++ ipcalc.c
@@ -0,0 +1,85 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+/**
+ * @file
+ *
+ * Copyright 2009 Digital Aggregates Corporation, Arvada CO 80001-0587 USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock <coverclock@diag.com><BR>
+ * http://www.diag.com/navigation/downloads/Diminuto.html<BR>
+ *
+ * USAGE
+ *
+ * ipcalc 192.168.1.222<BR>
+ * ipcalc 192.168.1.222 255.255.255.0<BR>
+ * ipcalc 192.168.1.222 255.255.255.0 255.255.255.255<BR>
+ * ipcalc 192.168.1.222 255.255.255.0 0.0.0.0<BR>
+ *
+ * EXAMPLES
+ *
+ * ABSTRACT
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+void usage(const char * pgm, FILE * fp)
+{
+    fprintf(fp, "Usage: %s 192.168.1.222\n", pgm);
+    fprintf(fp, "       %s 192.168.1.222 255.255.255.0\n", pgm);
+    fprintf(fp, "       %s 192.168.1.222 255.255.255.0 255.255.255.255\n", pgm);
+}
+
+int main(int argc, char ** argv)
+{
+    uint32_t addr[3];
+	int ii;
+    struct in_addr ip;
+
+    if ((argc == 2) && (strcmp(argv[1], "-?") == 0)) {
+        usage(argv[0], stdout);
+        return 0;
+    }
+
+    if (!((2 <= argc) && (argc <= 4))) {
+        usage(argv[0], stderr);
+        return 1;
+    }
+
+    for (ii = 1; ii < argc; ++ii) {
+        if (!inet_aton(argv[ii], &ip)) {
+            errno = EINVAL;
+            perror(argv[ii]);
+            return 1;
+        }
+        addr[ii - 1] = ip.s_addr;
+    }
+
+    if (argc == 4) {
+        
+        ip.s_addr = (addr[0] & addr[1]) | (addr[2] & (~addr[1]));
+
+    } else if (argc == 3) {
+
+        ip.s_addr = (addr[0] & addr[1]) | ((~0) ^ addr[1]);
+
+    } else {
+
+        addr[0] = ntohl(addr[0]);
+
+        if ((addr[0] & 0x80000000UL) == 0x00000000UL) {
+            ip.s_addr = htonl(0xff000000UL);
+        } else if ((addr[0] & 0xc0000000UL) == 0x80000000UL) {
+            ip.s_addr = htonl(0xffff0000UL);
+        } else {
+            ip.s_addr = htonl(0xffffff00UL);
+        }
+
+    }        
+
+    printf("%s\n", inet_ntoa(ip));
+
+    return 0;
+}
