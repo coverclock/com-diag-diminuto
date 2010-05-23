66006e15221a0484d50e5e3e44070624d4dcea2c
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2010-05-23 14:26:02 +0000

    Added endianess. Comments to dump about network byte order.
    git-svn-id: svn://graphite/diminuto/trunk/Diminuto@91 5dfbf39a-1452-0410-8a97-9e58fedde8e3

---
+++ diminuto_endianess.h
@@ -0,0 +1,30 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+#ifndef _H_COM_DIAG_DIMINUTO_ENDIANESS_
+#define _H_COM_DIAG_DIMINUTO_ENDIANESS_
+
+/**
+ * @file
+ *
+ * Copyright 2010 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock <coverclock@diag.com><BR>
+ * http://www.diag.com/navigation/downloads/Diminuto.html<BR>
+ */
+
+#include <stdint.h>
+
+/*
+ * I use a union now instead of the older trick of casting an integer pointer
+ * to a character pointer because it works reliably without regard to pointer
+ * aliasing assumptions made by compiler optimizers.
+ */
+static union { uint32_t word; uint8_t bytes[sizeof(uint32_t)]; } diminuto_endian = { 1 };
+
+/**
+ * Return true if the process on which this runs is little endian. I expect
+ * the compiler to trivially inline this.
+ * @return !0 if little endian, 0 if big endian.
+ */
+static int diminuto_littleendian(void) { return diminuto_endian.bytes[0]; }
+
+#endif
