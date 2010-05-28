4dde25f10206b4cdafb1a33058592a9c52eec06b
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2010-05-28 15:53:54 +0000

    Added fletcher8 and its unit test. Still debugging stacktrace, which I believe backtrace is broken for ARM in 2.6 thanks to EABI.
    git-svn-id: svn://graphite/diminuto/trunk@108 5dfbf39a-1452-0410-8a97-9e58fedde8e3

---
+++ Diminuto/diminuto_fletcher8.c
@@ -0,0 +1,23 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+/**
+ * @file
+ *
+ * Copyright 2010 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock <coverclock@diag.com><BR>
+ * http://www.diag.com/navigation/downloads/Concha.html<BR>
+ */
+
+#include "diminuto_fletcher8.h"
+
+void * diminuto_fletcher8(const void * buffer, size_t length, uint8_t * ap, uint8_t * bp)
+{
+	uint8_t * pp;
+
+	for (pp = (uint8_t *)buffer; length > 0; --length) {
+        *ap += *(pp++);
+        *bp += *ap;
+	}
+
+	return pp;
+}
---
+++ Diminuto/diminuto_fletcher8.h
@@ -0,0 +1,32 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+#ifndef _H_COM_DIAG_DIMINUTO_FLETCHER8_
+#define _H_COM_DIAG_DIMINUTO_FLETCHER8_
+
+/**
+ * @file
+ *
+ * Copyright 2010 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock <coverclock@diag.com><BR>
+ * http://www.diag.com/navigation/downloads/Diminuto.html<BR>
+ */
+
+#include <stdint.h>
+#include <sys/types.h>
+
+/**
+ * Compute a running Fletcher 8-bit checksum as described in RFC1146. Two
+ * checksum bytes are computed, the A byte and the B byte, The convention
+ * is for the caller to append the bytes (in that order) to the end of the
+ * checksummed data block. A running checksum is computed: the caller should
+ * initialize the A and B bytes to zero before calling the function at the
+ * beginning of the block to be checksummed.
+ * @param buffer points to the starting octet at which to begin checksumming.
+ * @param length is the number of octets over which to checksum.
+ * @param ap points to the A byte which must initially be zero.
+ * @param bp points to the B byte which must initially be zero.
+ * @return a pointer past the last checksummed octet.
+ */
+extern void * diminuto_fletcher8(const void * buffer, size_t length, uint8_t * ap, uint8_t * bp);
+
+#endif
