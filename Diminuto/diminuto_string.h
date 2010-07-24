5e3bf7840997799f7ac95201a51b908f1263b3b0
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2010-07-24 00:44:51 +0000

    Adding untested strscpy().
    git-svn-id: svn://graphite/diminuto/trunk@143 5dfbf39a-1452-0410-8a97-9e58fedde8e3

---
+++ Diminuto/diminuto_string.h
@@ -0,0 +1,29 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+#ifndef _H_COM_DIAG_DIMINUTO_STRING_
+#define _H_COM_DIAG_DIMINUTO_STRING_
+
+/**
+ * @file
+ *
+ * Copyright 2010 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock <coverclock@diag.com><BR>
+ * http://www.diag.com/navigation/downloads/Diminuto.html<BR>
+ */
+
+#include <sys/types.h>
+
+/**
+ * Copys no more than (n - 1) characters up to and including the
+ * terminating NUL character from a source character array
+ * to the destination character array. Guarantees that the
+ * destination character array is NUL terminated. Does not NUL
+ * fill the destination character array in the manner of strncpy().
+ * @param dest points to the destination character array.
+ * @param src points to the source character array.
+ * @param n is the size of the destination buffer.
+ * @return the pointer to the destination character array.
+ */
+extern char * diminuto_strscpy(char * dest, const char * src, size_t n);
+
+#endif
