0076c84bfa3b375e9ca698fa249c310daefcba9f
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2010-07-30 02:26:27 +0000

    Doxygen comments.
    git-svn-id: svn://graphite/diminuto/trunk/Diminuto@155 5dfbf39a-1452-0410-8a97-9e58fedde8e3

--- diminuto_pool.h
+++ diminuto_pool.h
@@ -16,12 +16,37 @@
 
 typedef diminuto_list diminuto_pool;
 
+/**
+ * Initialize a pool for objects of the specified size. The pool is initially
+ * empty. Do not initialize a pool that already has objects stored in it; that
+ * will result in a memory leak.
+ * @param poolp points to an uninitialized pool structure.
+ * @param size is the desired size of objects to be kept in the pool.
+ * @return a pointer to the pool.
+ */
 extern diminuto_pool * diminuto_pool_init(diminuto_pool * poolp, size_t size);
 
+/**
+ * Release all of the objects stored in the pool, freeing their memory.
+ * @param poolp points to a pool.
+ * @return a pointer to the pool.
+ */
 extern diminuto_pool * diminuto_pool_fini(diminuto_pool * poolp);
 
+/**
+ * Allocate an object from the pool. If an object is in the pool, it
+ * is removed from the pool and a pointer to it is returned. If the pool
+ * is empty, a new object of the correct size is allocated from the heap
+ * and a pointer to it is returned.
+ * @param poolp points to the pool.
+ * @return a pointer to an object of the desired size.
+ */
 extern void * diminuto_pool_alloc(diminuto_pool * poolp);
 
+/**
+ * Free an object back to the pool. The object is returned to the pool.
+ * @param pointer points to the object.
+ */
 extern void diminuto_pool_free(void * pointer);
 
 #endif
