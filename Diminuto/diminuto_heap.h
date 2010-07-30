be58f3d382ad4b37a6e05edbe57d808045c3af10
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2010-07-30 02:26:27 +0000

    Doxygen comments.
    git-svn-id: svn://graphite/diminuto/trunk@155 5dfbf39a-1452-0410-8a97-9e58fedde8e3

--- Diminuto/diminuto_heap.h
+++ Diminuto/diminuto_heap.h
@@ -9,6 +9,13 @@
  * Licensed under the terms in README.h<BR>
  * Chip Overclock <coverclock@diag.com><BR>
  * http://www.diag.com/navigation/downloads/Diminuto.html<BR>
+ *
+ * This simple little dependency injection mechanism allows you to replace
+ * malloc() and free() from <stdlib.h> with alternatives. I use this for
+ * unit testing, but you could just as easily install versions of those
+ * functions that track memory usage. (But go get the valgrind tool off
+ * the web if you don't already have it. It's the best thing since sliced
+ * bread.)
  */
 
 #include "diminuto_types.h"
@@ -21,15 +28,35 @@ typedef void (diminuto_heap_free_func)(void * pointer);
 extern diminuto_heap_alloc_func * diminuto_heap_alloc_funcp;
 extern diminuto_heap_free_func * diminuto_heap_free_funcp;
 
+/**
+ * diminuto_heap_alloc_set(_ALLOC_)
+ * Install an alternative heap memory allocation function pointed to
+ * by @a _ALLOC_.
+ */
 #define diminuto_heap_alloc_set(_ALLOC_) \
     (diminuto_heap_alloc_funcp = (_ALLOC_))
 
+/**
+ * diminuto_heap_free_set(_ALLOC_)
+ * Install an alternative heap memory free function pointed to
+ * by @a _ALLOC_.
+ */
 #define diminuto_heap_free_set(_FREE_) \
     (diminuto_heap_free_funcp = (_FREE_))
 
+/**
+ * diminuto_heap_alloc(_SIZE_)
+ * Allocate a block of memory of size @a _SIZE_ bytes from the heap and
+ * return its address or NULL if the allocation failed.
+ */
 #define diminuto_heap_alloc(_SIZE_) \
     ((*(diminuto_heap_alloc_funcp ? diminuto_heap_alloc_funcp : &malloc))(_SIZE_))
 
+/**
+ * diminuto_heap_free(_POINTER_)
+ * Free a block of previously allocated memory pointed to by @a _POINTER_
+ * back to the heap.
+ */
 #define diminuto_heap_free(_POINTER_) \
     ((*(diminuto_heap_free_funcp ? diminuto_heap_free_funcp : &free))(_POINTER_))
 
