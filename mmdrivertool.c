6b56291886e8acafda8e611adbcec26d2662eba3
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2011-01-24 19:19:58 +0000

    Finally, successfully tested memtool and mmdriver suite on BB using USER button.
    git-svn-id: svn://graphite/diminuto/trunk/Diminuto@165 5dfbf39a-1452-0410-8a97-9e58fedde8e3

--- mmdrivertool.c
+++ mmdrivertool.c
@@ -2,14 +2,14 @@
 /**
  * @file
  *
- * Copyright 2010 Digital Aggregates Corporation, Arvada CO 80001-0587 USA<BR>
+ * Copyright 2010-2011 Digital Aggregates Corporation, Colorado USA<BR>
  * Licensed under the terms in README.h<BR>
  * Chip Overclock <coverclock@diag.com><BR>
  * http://www.diag.com/navigation/downloads/Diminuto.html<BR>
  *
  * USAGE
  *
- * mmdrivertool [ -d ] [ -U DEVICE ] [ -[1|2|4|8] ADDRESS ] [ -[s|c|w] NUMBER | -r ] [ -u USECONDS ] [ ... ]
+ * mmdrivertool [ -d ] [ -U DEVICE ] [ -[1|2|4|8] ADDRESS ] [ -[s|S|c|C|w] NUMBER | -r ] [ -u USECONDS ] [ ... ]
  *
  * EXAMPLES
  *
@@ -64,11 +64,14 @@ static int control(int fd, int request, diminuto_mmdriver_op * opp)
 
 static void usage(void)
 {
-    fprintf(stderr, "usage: %s [ -d ] [ -U DEVICE ] [ -[1|2|4|8] OFFSET ] [ -[s|c|w] NUMBER ] [ -u USECONDS ] [ ... ]\n", program);
+    fprintf(stderr, "usage: %s [ -d ] [ -U DEVICE ] [ -[1|2|4|8] OFFSET ] [ -r | -[s|S|c|C|w] NUMBER ] [ -u USECONDS ] [ -t | -f ] [ ... ]\n", program);
     fprintf(stderr, "       -1 OFFSET   Use byte at OFFSET\n");
     fprintf(stderr, "       -2 OFFSET   Use halfword at OFFSET\n");
     fprintf(stderr, "       -4 OFFSET   Use word at OFFSET\n");
     fprintf(stderr, "       -8 OFFSET   Use doubleword at OFFSET\n");
+    fprintf(stderr, "       -C NUMBER   Clear 1<<NUMBER mask at OFFSET\n");
+    fprintf(stderr, "       -S NUMBER   Set 1<<NUMBER mask at OFFSET\n");
+    fprintf(stderr, "       -U DEVICE   Use DEVICE instead of %s\n", DIMINUTO_MMDRIVER_NODE);
     fprintf(stderr, "       -c NUMBER   Clear NUMBER mask at OFFSET\n");
     fprintf(stderr, "       -d          Enable debug mode\n");
     fprintf(stderr, "       -f          Proceed if the last result was 0\n");
@@ -77,7 +80,6 @@ static void usage(void)
     fprintf(stderr, "       -t          Proceed if the last result was !0\n");
     fprintf(stderr, "       -u USECONDS Sleep for USECONDS microseconds\n");
     fprintf(stderr, "       -w NUMBER   Write NUMBER to OFFSET\n");
-    fprintf(stderr, "       -U DEVICE   Use DEVICE instead of %s\n", DIMINUTO_MMDRIVER_NODE);
     fprintf(stderr, "       -?          Print menu\n");
 }
 
@@ -85,6 +87,7 @@ int main(int argc, char * argv[])
 {
     const char * device = DIMINUTO_MMDRIVER_NODE;
     uint64_t value = 0;
+    static const uint64_t bit = 1;
     int done = 0;
     int error = 0;
     int fd = -1;
@@ -100,7 +103,7 @@ int main(int argc, char * argv[])
     op.width = THIRTYTWO;
     op.datum.thirtytwo = 0;
 
-    while ((opt = getopt(argc, argv, "1:2:4:8:U:c:dfrs:tu:w:?")) >= 0) {
+    while ((opt = getopt(argc, argv, "1:2:4:8:C:S:U:c:dfrs:tu:w:?")) >= 0) {
 
         switch (opt) {
 
@@ -142,14 +145,15 @@ int main(int argc, char * argv[])
             device = optarg;
             break;
 
+        case 'C':
         case 'c':
             if (debug) { fprintf(stderr, "%s -%c %s\n", program, opt, optarg); }
             if (!(error = (*number(optarg, &value) != '\0'))) {
                 switch (op.width) {
-                case EIGHT: op.datum.eight = value; break;
-                case SIXTEEN: op.datum.sixteen = value;  break;
-                case THIRTYTWO: op.datum.thirtytwo = value; break;
-                case SIXTYFOUR: op.datum.sixtyfour = value;  break;
+                case EIGHT:		op.datum.eight		= (opt == 'C') ? value : bit << value; break;
+                case SIXTEEN:	op.datum.sixteen	= (opt == 'C') ? value : bit << value; break;
+                case THIRTYTWO:	op.datum.thirtytwo	= (opt == 'C') ? value : bit << value; break;
+                case SIXTYFOUR:	op.datum.sixtyfour	= (opt == 'C') ? value : bit << value; break;
                 }
                 if (!(error = ((fd = diminuto_fd_acquire(fd, device, O_RDWR, 0)) < 0))) {
                     error = control(fd, DIMINUTO_MMDRIVER_CLEAR, &op) < 0;
@@ -165,10 +169,10 @@ int main(int argc, char * argv[])
         case 'f':
             if (debug) { fprintf(stderr, "%s -%c\n", program, opt); }
             switch (op.width) {
-            case EIGHT: done = op.datum.eight != 0; break;
-            case SIXTEEN: done = op.datum.sixteen != 0;  break;
-            case THIRTYTWO: done = op.datum.thirtytwo != 0; break;
-            case SIXTYFOUR: done = op.datum.sixtyfour != 0;  break;
+            case EIGHT:		done = op.datum.eight		!= 0;	break;
+            case SIXTEEN:	done = op.datum.sixteen		!= 0;	break;
+            case THIRTYTWO:	done = op.datum.thirtytwo	!= 0;	break;
+            case SIXTYFOUR:	done = op.datum.sixtyfour	!= 0;	break;
             }
             break;
 
@@ -177,23 +181,24 @@ int main(int argc, char * argv[])
             if (!(error = ((fd = diminuto_fd_acquire(fd, device, O_RDWR, 0)) < 0))) {
                 if (!(error = control(fd, DIMINUTO_MMDRIVER_READ, &op) < 0)) {
                     switch (op.width) {
-                    case EIGHT: printf("%u\n", op.datum.eight); break;
-                    case SIXTEEN: printf("%u\n", op.datum.sixteen);  break;
-                    case THIRTYTWO: printf("%u\n", op.datum.thirtytwo); break;
-                    case SIXTYFOUR: printf("%llu\n", op.datum.sixtyfour);  break;
+                    case EIGHT:		printf("%u\n",		op.datum.eight);		break;
+                    case SIXTEEN:	printf("%u\n",		op.datum.sixteen);		break;
+                    case THIRTYTWO:	printf("%u\n",		op.datum.thirtytwo);	break;
+                    case SIXTYFOUR:	printf("%llu\n",	op.datum.sixtyfour);	break;
                     }
                 }
             }
             break;
 
+        case 'S':
         case 's':
             if (debug) { fprintf(stderr, "%s -%c %s\n", program, opt, optarg); }
             if (!(error = (*number(optarg, &value) != '\0'))) {
                 switch (op.width) {
-                case EIGHT: op.datum.eight = value; break;
-                case SIXTEEN: op.datum.sixteen = value;  break;
-                case THIRTYTWO: op.datum.thirtytwo = value; break;
-                case SIXTYFOUR: op.datum.sixtyfour = value;  break;
+                case EIGHT:		op.datum.eight		= (opt == 'S') ? value : bit << value; break;
+                case SIXTEEN:	op.datum.sixteen	= (opt == 'S') ? value : bit << value; break;
+                case THIRTYTWO:	op.datum.thirtytwo	= (opt == 'S') ? value : bit << value; break;
+                case SIXTYFOUR:	op.datum.sixtyfour	= (opt == 'S') ? value : bit << value; break;
                 }
                 if (!(error = ((fd = diminuto_fd_acquire(fd, device, O_RDWR, 0)) < 0))) {
                     error = control(fd, DIMINUTO_MMDRIVER_SET, &op) < 0;
@@ -204,10 +209,10 @@ int main(int argc, char * argv[])
         case 't':
             if (debug) { fprintf(stderr, "%s -%c\n", program, opt); }
             switch (op.width) {
-            case EIGHT: done = op.datum.eight == 0; break;
-            case SIXTEEN: done = op.datum.sixteen == 0;  break;
-            case THIRTYTWO: done = op.datum.thirtytwo == 0; break;
-            case SIXTYFOUR: done = op.datum.sixtyfour == 0;  break;
+            case EIGHT:		done = op.datum.eight		== 0; break;
+            case SIXTEEN:	done = op.datum.sixteen		== 0; break;
+            case THIRTYTWO:	done = op.datum.thirtytwo	== 0; break;
+            case SIXTYFOUR:	done = op.datum.sixtyfour	== 0; break;
             }
             break;
 
@@ -222,10 +227,10 @@ int main(int argc, char * argv[])
             if (debug) { fprintf(stderr, "%s -%c %s\n", program, opt, optarg); }
             if (!(error = (*number(optarg, &value) != '\0'))) {
                 switch (op.width) {
-                case EIGHT: op.datum.eight = value; break;
-                case SIXTEEN: op.datum.sixteen = value;  break;
-                case THIRTYTWO: op.datum.thirtytwo = value; break;
-                case SIXTYFOUR: op.datum.sixtyfour = value;  break;
+                case EIGHT:		op.datum.eight		= value; break;
+                case SIXTEEN:	op.datum.sixteen	= value; break;
+                case THIRTYTWO:	op.datum.thirtytwo	= value; break;
+                case SIXTYFOUR:	op.datum.sixtyfour	= value; break;
                 }
                 if (!(error = ((fd = diminuto_fd_acquire(fd, device, O_RDWR, 0)) < 0))) {
                     error = control(fd, DIMINUTO_MMDRIVER_WRITE, &op) < 0;
