eae1e3cc591ef9f5095a21dca2aab6bae2748d4b
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2013-07-04 18:01:29 +0000

    1. Makefile now defaults to just building library, tools, unit tests. 2. phex functions broken out into separate functions. 3. tokenizing macros broken out from version to separate header. 4. version adds vintage string. 5. version now allows date, time to be overridden (and vintage too).
    git-svn-id: svn://graphite/diminuto/trunk/Diminuto@181 5dfbf39a-1452-0410-8a97-9e58fedde8e3

--- include/com/diag/diminuto/diminuto_version.h
+++ include/com/diag/diminuto/diminuto_version.h
@@ -5,20 +5,35 @@
 /**
  * @file
  *
- * Copyright 2010 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Copyright 2010-2013 Digital Aggregates Corporation, Colorado, USA<BR>
  * Licensed under the terms in README.h<BR>
  * Chip Overclock <coverclock@diag.com><BR>
  * http://www.diag.com/navigation/downloads/Diminuto.html<BR>
  *
  * Inserts a build stamp into the translation unit into which it is included
  * that can be extracted from the resulting object code using command line
- * tools like "string".
+ * tools like "string". Allows the date and time stamp provided by the C
+ * compiler to be overridden from the command line to accommodate folks who
+ * get nervous when two successive builds do not produce the identically same
+ * binary. Remarkably, such paranoid folks includes certification agencies like
+ * the FAA. Oh how many times have I wished that __DATE__ was in ISO8601 format
+ * and __TIME__ was in 24-hour UTC.
  */
 
-#define DIMINUTO_VERSION_STRING(_ARG_) #_ARG_
+#include "com/diag/diminuto/diminuto_token.h"
 
-#define DIMINUTO_VERSION_LINE(_ARG_) DIMINUTO_VERSION_STRING(_ARG_)
+#if !defined(DIMINUTO_VERSION_VINTAGE)
+#	define DIMINUTO_VERSION_VINTAGE 0.0.0.0
+#endif
 
-static const char DIMINUTO_VERSION[] __attribute__((__used__)) = "DIMINUTO_VERSION=" __FILE__ " " DIMINUTO_VERSION_LINE(__LINE__) " " __DATE__ " " __TIME__;
+#if !defined(DIMINUTO_VERSION_DATE)
+#	define DIMINUTO_VERSION_DATE __DATE__
+#endif
+
+#if !defined(DIMINUTO_VERSION_TIME)
+#	define DIMINUTO_VERSION_TIME __TIME__
+#endif
+
+static const char DIMINUTO_VERSION[] __attribute__((__used__)) = "DIMINUTO_VERSION=" DIMINUTO_TOKEN_TOKEN(DIMINUTO_VERSION_VINTAGE) "|" DIMINUTO_VERSION_DATE "|" DIMINUTO_VERSION_TIME;
 
 #endif
