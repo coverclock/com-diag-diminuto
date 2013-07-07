8c5b30857325afae22cf527447a961beebec5557
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2013-07-07 22:55:41 +0000

    First cut at well (which builds but is untested).
    git-svn-id: svn://graphite/diminuto/trunk@199 5dfbf39a-1452-0410-8a97-9e58fedde8e3

---
+++ Diminuto/diminuto_well.c
@@ -0,0 +1,167 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+/**
+ * @file
+ *
+ * Copyright 2013 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock <coverclock@diag.com><BR>
+ * http://www.diag.com/navigation/downloads/Diminuto.html<BR>
+ */
+
+#include "com/diag/diminuto/diminuto_well.h"
+#include "com/diag/diminuto/diminuto_heap.h"
+#include "com/diag/diminuto/diminuto_log.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+static const ssize_t PAGESIZE = 4096;
+static const ssize_t LINESIZE = 64;
+
+static const size_t FREE = 0;
+static const size_t USED = 1;
+
+ssize_t diminuto_well_pagesize()
+{
+	ssize_t pagesize;
+
+#if defined(DIMINUTO_PAGESIZE)
+	pagesize = DIMINUTO_PAGESIZE;
+#elif defined(_SC_PAGESIZE)
+	if ((pagesize = sysconf(_SC_PAGESIZE)) < 0) {
+		pagesize = PAGESIZE;
+	}
+#else
+	if ((pagesize = getpagesize()) < 0) {
+		pagesize = PAGESIZE;
+	}
+#endif
+
+	return pagesize;
+}
+
+ssize_t diminuto_well_linesize()
+{
+	ssize_t linesize;
+
+#if defined(DIMINUT_LINESIZE)
+	linesize = DIMINUTO_LINESIZE;
+#elif defined(_SC_LEVEL1_DCACHE_LINESIZE)
+	if ((linesize = sysconf(_SC_LEVEL1_DCACHE_LINESIZE)) < 0) {
+		linesize = LINESIZE;
+	}
+#else
+	FILE * fp;
+	if ((fp = fopen("/sys/devices/system/cpu/cpu0/cache/index0/coherency_line_size", "r")) != (FILE *)0) {
+		if (ffscanf(fp, "%zd", &linesize) == 0) {
+			linesize = LINESIZE;
+		}
+	} else if ((fp = fopen("/sys/devices/system/cpu/cpu0/cache/coherency_line_size", "r")) != (FILE *)0) {
+		if (ffscanf(fp, "%zd", &linesize) == 0) {
+			linesize = LINESIZE;
+		}
+	} else {
+		linesize = LINESIZE;
+	}
+#endif
+
+	return linesize;
+}
+
+diminuto_well_t * diminuto_well_init(size_t size, size_t count)
+{
+	diminuto_well_t * wellp = (diminuto_well_t *)0;
+	diminuto_well_t * well;
+	size_t pagesize;
+	int rc;
+	void * control = (void *)0;
+	void * data = (void *)0;
+	size_t ii;
+
+	do {
+
+		pagesize = diminuto_well_pagesize();
+		if (pagesize < 0) {
+			errno = EINVAL;
+			diminuto_perror("diminuto_well_pagesize");
+			break;
+		}
+
+		rc = posix_memalign(&control, pagesize, sizeof(diminuto_list_t) * (2 + count));
+		if (rc != 0) {
+			errno = rc;
+			diminuto_perror("posix_memalign(control)");
+			break;
+		}
+
+		rc = posix_memalign(&data, pagesize, size * count);
+		if (rc != 0) {
+			errno = rc;
+			diminuto_perror("posix_memalign(data)");
+			free(control);
+			break;
+		}
+
+		well = (diminuto_well_t *)control;
+		diminuto_list_datainit(&well[FREE], control);
+		diminuto_list_datainit(&well[USED], data);
+		for (ii = 0; ii < count; ++ii) {
+			diminuto_list_insert(&well[FREE], diminuto_list_datainit(&well[ii + 2], data));
+			data += size;
+		}
+
+		wellp = &well[0];
+
+	} while (0);
+
+	return wellp;
+}
+
+void diminuto_well_fini(diminuto_well_t * wellp)
+{
+	free(diminuto_list_data(&wellp[USED])); /* data */
+	free(diminuto_list_data(&wellp[FREE])); /* control */
+}
+
+void * diminuto_well_alloc(diminuto_well_t * wellp)
+{
+    void * pointer = (void *)0;
+    diminuto_list_t * nodep;
+
+    wellp = diminuto_list_root(wellp);
+    if (!diminuto_list_isempty(&wellp[FREE])) {
+        nodep = diminuto_list_remove(diminuto_list_next(&wellp[FREE]));
+        if (nodep != (diminuto_well_t *)0) {
+        	pointer = diminuto_list_data(nodep);
+        	diminuto_list_insert(diminuto_list_prev(&wellp[USED]), nodep);
+        } else {
+        	errno = EFAULT;
+        }
+    } else {
+    	errno = ENOMEM;
+    }
+
+    return pointer;
+}
+
+int diminuto_well_free(diminuto_well_t * wellp, void * pointer)
+{
+	int rc = -1;
+    diminuto_list_t * nodep;
+
+    wellp = diminuto_list_root(wellp);
+    if (!diminuto_list_isempty(&wellp[USED])) {
+    	nodep = diminuto_list_remove(diminuto_list_next(&wellp[USED]));
+    	if (nodep != (diminuto_well_t *)0) {
+    		diminuto_list_insert(diminuto_list_prev(&wellp[FREE]), diminuto_list_datainit(nodep, pointer));
+    		rc = 0;
+    	} else {
+    		errno = EFAULT;
+    	}
+    } else {
+    	errno = ENOMEM;
+    }
+
+    return rc;
+}
