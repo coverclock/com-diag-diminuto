76ef809633dd82783893f0b5fbf491faaa35caa3
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2013-08-21 03:08:40 +0000

    9.6.5.0
    git-svn-id: svn://graphite/diminuto/trunk@236 5dfbf39a-1452-0410-8a97-9e58fedde8e3

---
+++ Diminuto/Well.cpp
@@ -0,0 +1,135 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+/**
+ * @file
+ *
+ * Copyright 2013 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock <coverclock@diag.com><BR>
+ * http://www.diag.com/navigation/downloads/Diminuto.html<BR>
+ *
+ * These are the classes used to integrate Diminuto C-based wells into C++.
+ * The are not stored in the Diminuto shared object because to do so,
+ * apparently, requires that all of the applications that link with the Diminuto
+ * shared object (like all of the unit tests) have C++ main programs. So this
+ * is a temporary fix until I figure out how I want to handle this (a second
+ * library, another project, etc.).
+ */
+
+#include "com/diag/diminuto/diminuto_well.h"
+
+namespace com {
+ namespace diag {
+  namespace diminuto {
+
+/*******************************************************************************
+ * BASE WELL
+ ******************************************************************************/
+
+BaseWell::BaseWell(size_t ss, size_t cc, bool pp, size_t aa)
+: size(ss)
+, cardinality(cc)
+, alignment(aa)
+, wellp(static_cast<diminuto_well_t *>(0))
+{
+	if (cc == 0) {
+		/* Do nothing. */
+	} else if (!pp) {
+		/* Do nothing. */
+	} else {
+		init(size, cardinality, alignment);
+	}
+}
+
+BaseWell::~BaseWell() {
+	fini();
+}
+
+void BaseWell::init(size_t ss, size_t cc, size_t aa) {
+	if (wellp == static_cast<diminuto_well_t *>(0)) {
+		wellp = diminuto_well_init(ss, cc, aa);
+	}
+}
+
+void BaseWell::init() {
+	init(size, cardinality, alignment);
+}
+
+void BaseWell::fini() {
+	if (wellp == static_cast<diminuto_well_t *>(0)) {
+		/* Do nothing. */
+	} else if (diminuto_well_isfull(wellp)) {
+		diminuto_well_fini(wellp);
+		wellp = (diminuto_well_t *)0;
+	} else {
+		/* Do nothing. */
+	}
+}
+
+void * BaseWell::alloc() {
+	void * that;
+	init();
+	that = diminuto_well_alloc(wellp);
+	return that;
+}
+
+void BaseWell::free(void * pointer) {
+	diminuto_well_free(wellp, pointer);
+}
+
+bool BaseWell::isFull() const {
+	return (diminuto_well_isfull(wellp) != 0);
+}
+
+bool BaseWell::isEmpty() const {
+	return (diminuto_well_isempty(wellp) != 0);
+}
+
+/*******************************************************************************
+ * SAFE BASE WELL
+ ******************************************************************************/
+
+SafeBaseWell::SafeBaseWell(size_t ss, size_t cc, bool pp, size_t aa)
+: BaseWell(ss, cc, pp, aa)
+{
+	pthread_mutex_init(&mutex, (pthread_mutexattr_t *)0);
+}
+
+SafeBaseWell::~SafeBaseWell() {
+	pthread_mutex_destroy(&mutex);
+}
+
+void SafeBaseWell::init() {
+	pthread_mutex_lock(&mutex);
+		BaseWell::init();
+	pthread_mutex_unlock(&mutex);
+}
+
+void SafeBaseWell::init(size_t ss, size_t cc, size_t aa) {
+	pthread_mutex_lock(&mutex);
+		BaseWell::init(ss, cc, aa);
+	pthread_mutex_unlock(&mutex);
+}
+
+void SafeBaseWell::fini() {
+	pthread_mutex_lock(&mutex);
+		BaseWell::fini();
+	pthread_mutex_unlock(&mutex);
+}
+
+void * SafeBaseWell::alloc() {
+	void * that;
+	pthread_mutex_lock(&mutex);
+		that = BaseWell::alloc();
+	pthread_mutex_unlock(&mutex);
+	return that;
+}
+
+void SafeBaseWell::free(void * pointer) {
+	pthread_mutex_lock(&mutex);
+		BaseWell::free(pointer);
+	pthread_mutex_unlock(&mutex);
+}
+
+  }
+ }
+}
