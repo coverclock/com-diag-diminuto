6a994945591226b620f374e167ba1b890a47a967
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2013-08-23 01:52:02 +0000

    Checkpoint.
    git-svn-id: svn://graphite/diminuto/trunk@239 5dfbf39a-1452-0410-8a97-9e58fedde8e3

--- Diminuto/diminuto_escape.c
+++ Diminuto/diminuto_escape.c
@@ -148,6 +148,7 @@ size_t diminuto_escape_expand(char * to, const char * from, size_t tsize, size_t
     const char * ff;
     char * tt;
     size_t ss;
+    const char * ee;
 
     ff = from;
     tt = to;
@@ -155,16 +156,16 @@ size_t diminuto_escape_expand(char * to, const char * from, size_t tsize, size_t
     while (0 < fsize) {
         if (tsize <= (ss = 1)) {
         	break;
-        } else if ((' ' <= *ff) && (*ff <= '~')) {
-    		*(tt++) = *ff;              /* Non-escaped character. */
+        } else if ((' ' <= *ff) && (*ff <= '~') && (((const char *)0 == special) || ((const char *)0 == (ee = strchr(special, *ff))))) {
+    		*(tt++) = *ff;              /* Unescaped character. */
         } else if (tsize <= (ss = 2)) {
         	break;
+        } else if ((const char *)0 != ee) {
+            *(tt++) = '\\';             /* Special character. */
+            *(tt++) = *ff;
     	} else if ('\0' == *ff) {
             *(tt++) = '\\';             /* Escaped zero a.k.a. NUL. */
             *(tt++) = '0';
-        } else if (((const char *)0 != special) && (0 != strchr(special, *ff))) {
-            *(tt++) = '\\';             /* Escaped character. */
-            *(tt++) = *ff;
         } else if ('\a' == *ff) {
             *(tt++) = '\\';             /* Alarm a.k.a. BEL. */
             *(tt++) = 'a';
@@ -186,7 +187,7 @@ size_t diminuto_escape_expand(char * to, const char * from, size_t tsize, size_t
         } else if ('\v' == *ff) {
             *(tt++) = '\\';             /* Vertical tab a.k.a. VT. */
             *(tt++) = 'v';
-        } else if (tsize <= (ss = 4)) {
+       } else if (tsize <= (ss = 4)) {
         	break;
         } else {
             *(tt++) = '\\';             /* Hexadecimal sequence. */
---
+++ Diminuto/unittest-escape.c
@@ -0,0 +1,35 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+/**
+ * @file
+ *
+ * Copyright 2010 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock <coverclock@diag.com><BR>
+ * http://www.diag.com/navigation/downloads/Diminuto.html<BR>
+ */
+
+#include "com/diag/diminuto/diminuto_escape.h"
+#include "com/diag/diminuto/diminuto_dump.h"
+#include "com/diag/diminuto/diminuto_unittest.h"
+#include <string.h>
+#include <stdio.h>
+
+int main(void)
+{
+	{
+		const char ONE[]="\0\a\b\f\n\r\t\v\1\23\123\xa\xBC";
+		char two[64];
+		char three[64];
+		size_t size;
+
+		diminuto_dump(stdout, ONE, sizeof(ONE));
+		size = diminuto_escape_expand(two, ONE, sizeof(two), sizeof(ONE), (const char *)0);
+		printf("\"%s\"\n", two);
+		size = diminuto_escape_collapse(three, two, strlen(two));
+		diminuto_dump(stdout, three, size);
+
+	}
+
+
+    EXIT();
+}
