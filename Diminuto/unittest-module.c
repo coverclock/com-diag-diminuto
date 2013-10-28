f516901c00eaa420bb26abec0a70e105f1a3b8e3
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2013-10-28 20:44:49 +0000

    First cut at modules works!
    git-svn-id: svn://graphite/diminuto/trunk@286 5dfbf39a-1452-0410-8a97-9e58fedde8e3

--- Diminuto/unittest-module.c
+++ Diminuto/unittest-module.c
@@ -21,6 +21,9 @@ int main(int argc, char ** argv)
 {
 	diminuto_module_handle_t module;
 	diminuto_module_handle_t check;
+	void * functionp;
+	int value = 0xa5a5a5a5;
+	int rc;
 
     diminuto_core_enable();
 
@@ -33,11 +36,21 @@ int main(int argc, char ** argv)
     check = diminuto_module_handle(MODULE);
     EXPECT(check == module);
 
+    functionp = diminuto_module_symbol(module, "diminuto_module_example", (const char *)0);
+    ASSERT(functionp != (void *)0);
+
+    rc = (*(int (*)(int))functionp)(value);
+    printf("0x%8.8x=(*%p)(0x%8.8x)\n", rc, functionp, value);
+    EXPECT(rc == ~value);
+
+
     module = diminuto_module_unload(module);
     EXPECT(module == (diminuto_module_handle_t)0);
 
+    /*
     check = diminuto_module_handle(MODULE);
     EXPECT(check == (diminuto_module_handle_t)0);
+    */
 
     return 0;
 }
