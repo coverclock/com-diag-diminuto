4d0a5b3d3fc2261ee3ec39065aab0bd48c6efff3
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2013-11-03 00:50:59 +0000

    Comments.
    git-svn-id: svn://graphite/diminuto/trunk/Diminuto@301 5dfbf39a-1452-0410-8a97-9e58fedde8e3

--- include/com/diag/diminuto/diminuto_pin.h
+++ include/com/diag/diminuto/diminuto_pin.h
@@ -13,14 +13,43 @@
 
 #include <stdio.h>
 
+/**
+ * Place the feature in debug mode in which the functions write to files
+ * in the /tmp directory instead of in /sys.
+ * @param enable if true places feature in debug mode, otherwise in run mode.
+ * @return the prior mode.
+ */
 extern int diminuto_pin_debug(int enable);
 
+/**
+ * Return a FILE pointer for the specified GPIO pin configured for as an input.
+ * @param pin identifies the pin by number from the data sheet.
+ * @return a FILE pointer or NULL if error.
+ */
 extern FILE * diminuto_pin_input(int pin);
 
+/**
+ * Return a FILE pointer for the specified GPIO pin configured for as an output.
+ * @param pin identifies the pin by number from the data sheet.
+ * @return a FILE pointer or NULL if error.
+ */
 extern FILE * diminuto_pin_output(int pin);
 
-extern int diminuto_pin_set(FILE * fp, int assert);
-
+/**
+ * Return the value of a GPIO pin, true (high) or false (low). The application
+ * is responsible for inversion (if, for example, the pin is active low).
+ * @param fp points to an input GPIO FILE pointer.
+ * @return >0 for high, 0 for low, <0 for error.
+ */
 extern int diminuto_pin_get(FILE * fp);
+
+/**
+ * Set a GPIO pin to high (true) or low (false). The application is responsible
+ * for inversion (if, for example, the pin is active low).
+ * @param fp points to an output GPIO FILE pointer.
+ * @param assert is true to set the pin to high, false to set it to low.
+ * @return >=0 for success, <0 for error.
+ */
+extern int diminuto_pin_set(FILE * fp, int assert);
 
 #endif
