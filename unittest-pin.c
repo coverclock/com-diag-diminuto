76ef83b7c57b98bf50150afadc99e332dc96cd01
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2013-11-03 22:48:57 +0000

    checkpoint while unit testing.
    git-svn-id: svn://graphite/diminuto/trunk/Diminuto@302 5dfbf39a-1452-0410-8a97-9e58fedde8e3

--- unittest-pin.c
+++ unittest-pin.c
@@ -14,10 +14,10 @@
 
 int main(void)
 {
-	FILE * fp;
 	FILE * pin98;
 	FILE * pin99;
 
+	EXPECT(system("rm -rf /tmp/class/gpio") >= 0);
 	EXPECT(system("mkdir -p /tmp/class/gpio") >= 0);
 	EXPECT(system("mkdir -p /tmp/class/gpio/gpio98") >= 0);
 	EXPECT(system("mkdir -p /tmp/class/gpio/gpio99") >= 0);
@@ -30,14 +30,33 @@ int main(void)
 	EXPECT(diminuto_pin_debug(!0) == 0);
 
 	ASSERT((pin98 = diminuto_pin_input(98)) != (FILE *)0);
-	ASSERT((pin99 = diminuto_pin_output(99)) != (FILE *)0);
+	EXPECT(system("test `cat /tmp/class/gpio/export` == 98") == 0);
+	EXPECT(system("test `cat /tmp/class/gpio/gpio98/direction` == input") == 0);
+	EXPECT(system("test `cat /tmp/class/gpio/gpio98/direction` == output") != 0);
 
-	EXPECT(diminuto_pin_get(pin98) >= 0);
+	ASSERT((pin99 = diminuto_pin_output(99)) != (FILE *)0);
+	EXPECT(system("test `cat /tmp/class/gpio/export` == 99") == 0);
+	EXPECT(system("test `cat /tmp/class/gpio/gpio99/direction` == output") == 0);
+	EXPECT(system("test `cat /tmp/class/gpio/gpio99/direction` == input") != 0);
+
+	EXPECT(system("echo 0 > /tmp/class/gpio/gpio98/value") == 0);
+	EXPECT(!diminuto_pin_get(pin98));
+	EXPECT(system("echo 1 > /tmp/class/gpio/gpio98/value") == 0);
+	EXPECT(!!diminuto_pin_get(pin98));
+	EXPECT(system("echo 0 > /tmp/class/gpio/gpio98/value") == 0);
+	EXPECT(!diminuto_pin_get(pin98));
+
 	EXPECT(diminuto_pin_set(pin99, 0) >= 0);
+	EXPECT(system("test `cat /tmp/class/gpio/gpio99/value` == 0") == 0);
 	EXPECT(diminuto_pin_set(pin99, !0) >= 0);
+	EXPECT(system("test `cat /tmp/class/gpio/gpio99/value` == 1") == 0);
+	EXPECT(diminuto_pin_set(pin99, 0) >= 0);
+	EXPECT(system("test `cat /tmp/class/gpio/gpio99/value` == 0") == 0);
 
 	EXPECT(fclose(pin98) != EOF);
 	EXPECT(fclose(pin99) != EOF);
+
+	EXPECT(diminuto_pin_debug(0) == !0);
 
 	EXIT();
 }
