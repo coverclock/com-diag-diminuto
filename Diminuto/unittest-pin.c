ed5bf7bbf66fd720cbefd24094ec654959f89991
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2013-11-03 00:43:14 +0000

    First cut at unit test success.
    git-svn-id: svn://graphite/diminuto/trunk@300 5dfbf39a-1452-0410-8a97-9e58fedde8e3

--- Diminuto/unittest-pin.c
+++ Diminuto/unittest-pin.c
@@ -10,12 +10,22 @@
 
 #include "com/diag/diminuto/diminuto_unittest.h"
 #include "com/diag/diminuto/diminuto_pin.h"
+#include <stdlib.h>
 
 int main(void)
 {
 	FILE * fp;
 	FILE * pin98;
 	FILE * pin99;
+
+	EXPECT(system("mkdir -p /tmp/class/gpio") >= 0);
+	EXPECT(system("mkdir -p /tmp/class/gpio/gpio98") >= 0);
+	EXPECT(system("mkdir -p /tmp/class/gpio/gpio99") >= 0);
+	EXPECT(system("touch /tmp/class/gpio/export") >= 0);
+	EXPECT(system("touch /tmp/class/gpio/gpio98/direction") >= 0);
+	EXPECT(system("touch /tmp/class/gpio/gpio98/value") >= 0);
+	EXPECT(system("touch /tmp/class/gpio/gpio99/direction") >= 0);
+	EXPECT(system("touch /tmp/class/gpio/gpio99/value") >= 0);
 
 	EXPECT(diminuto_pin_debug(!0) == 0);
 
