e5ec85c2c985ba4b1d5dcbc69805c95d45655a94
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2013-11-23 23:07:41 +0000

    Added (unexpanded) mux unit test. Expanded ipc unit test. Ipc unit test works.
    git-svn-id: svn://graphite/diminuto/trunk/Diminuto@365 5dfbf39a-1452-0410-8a97-9e58fedde8e3

---
+++ tst/unittest-mux.c
@@ -0,0 +1,21 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+/**
+ * @file
+ *
+ * Copyright 2013 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock (coverclock@diag.com)<BR>
+ * http://www.diag.com/navigation/downloads/Diminuto.html<BR>
+ */
+
+#include "com/diag/diminuto/diminuto_unittest.h"
+#include "com/diag/diminuto/diminuto_core.h"
+#include "com/diag/diminuto/diminuto_mux.h"
+#include "com/diag/diminuto/diminuto_ipc.h"
+#include <errno.h>
+
+int main(int argc, char ** argv)
+{
+
+    EXIT();
+}
