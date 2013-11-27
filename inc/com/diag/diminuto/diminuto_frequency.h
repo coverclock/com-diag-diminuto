fdf11bac9cc4731dad76f0d7ae5a23fb42919f8b
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2013-11-27 21:05:21 +0000

    Comment change.
    git-svn-id: svn://graphite/diminuto/trunk/Diminuto@390 5dfbf39a-1452-0410-8a97-9e58fedde8e3

--- inc/com/diag/diminuto/diminuto_frequency.h
+++ inc/com/diag/diminuto/diminuto_frequency.h
@@ -9,8 +9,6 @@
  * Licensed under the terms in README.h<BR>
  * Chip Overclock <coverclock@diag.com><BR>
  * http://www.diag.com/navigation/downloads/Diminuto.html<BR>
- *
- * This is _not_ part of the public interface.
  */
 
 #include "com/diag/diminuto/diminuto_types.h"
