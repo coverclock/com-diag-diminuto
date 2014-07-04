ec67a14a6f65c2ece62949e3342fd9bcb37eda0d
Chip Overclock <coverclock@diag.com>
2014-07-04 12:15:27 -0600

    Trying to fix up dir mixup.

--- etc/dlib.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-# Copyright 2008 Digital Aggregates Corporation, Arvada CO 80001-0587 USA
-# Licensed under the terms in README.h
-# Chip Overclock <coverclock@diag.com>
-
-. `which diminuto`
-
-export LD_LIBRARY_PATH=$(TOOLCHAIN)/lib:$(TOOLCHAIN)/usr/arm-linux-uclibc/lib:$(TOOLCHAIN)/usr/lib:$(TOOLCHAIN)/usr/local/lib:$(DESPERADO):$(FICL)
