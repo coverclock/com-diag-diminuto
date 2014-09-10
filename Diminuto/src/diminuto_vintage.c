80519e650673db5bb4a6786581fdeda5cd0acc10
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2014-09-10 19:55:25 +0000

    Added release and vintage translation units just to embed release and vintage strings in archive or shared object.
    git-svn-id: svn://graphite/diminuto/trunk@473 5dfbf39a-1452-0410-8a97-9e58fedde8e3

---
+++ Diminuto/src/diminuto_vintage.c
@@ -0,0 +1,14 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+/**
+ * @file
+ *
+ * Copyright 2014 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock (coverclock@diag.com)<BR>
+ * http://www.diag.com/navigation/downloads/Diminuto.html<BR>
+ * The purpose of this translation unit is to embed the vintage string
+ * inside the library or shared object. Since the resulting object module
+ * has no external references, it shouldn't ever be linked into an application.
+ */
+
+#include "com/diag/diminuto/diminuto_vintage.h"
