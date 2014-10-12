1719fd445ca9c270fe0f8a4125ee83cdb77738a1
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2014-10-12 21:36:47 +0000

    Wipe looking pretty good testing on USB stick.
    git-svn-id: svn://graphite/diminuto/trunk@484 5dfbf39a-1452-0410-8a97-9e58fedde8e3

--- Diminuto/bin/wipe.c
+++ Diminuto/bin/wipe.c
@@ -23,47 +23,170 @@
 #include <stdint.h>
 #include <string.h>
 #include <malloc.h>
-#include <errno.h>
+#include <time.h>
 #include <errno.h>
 
-int main(int argc, void ** argv)
+int main(int argc, char ** argv)
 {
-    ssize_t alignment = 0;
+    int xc = 1;
+    ssize_t pagesize = 0;
     void * buffer = 0;
     int fd = -1;
     ssize_t rc = 0;
-    uint64_t sn = 0;
+    size_t size = 0;
+    char * pointer = 0;
+    uint64_t datum = 0;
+    uint64_t * data = 0;
+    uint64_t total = 0;
+    uint64_t errors = 0;
+    long base = 0;
+    time_t start = 0;
+    time_t end = 0;
+    int error = 0;
 
     do {
 
         if (argc <= 1) {
-            perror("usage");
+            fprintf(stderr, "usage: %s DEVICE\n", argv[0]);
             break;
         }
 
-        alignment = getpagesize();
-        if (alignment <= 0) {
+        base = random();
+
+        pagesize = getpagesize();
+        if (pagesize <= 0) {
             perror("getpagesize");
             break;
         }
 
-        buffer = memalign(alignment, alignment);
+        buffer = memalign(pagesize, pagesize);
         if (buffer == 0) {
             perror("memalign");
             break;
         }
 
-        fd = open(argv[1], O_DIRECT | O_SYNC, 0);
+        fd = open(argv[1], O_WRONLY | O_DIRECT | O_SYNC, 0);
         if (fd < 0) {
             perror(argv[1]);
             break;
         }
 
+        datum = base;
+        total = 0;
+        start = time(0);
+
+        do {
+
+            data = (uint64_t *)buffer;
+            size = pagesize / sizeof(datum);
+            while ((size--) > 0) {
+                *(data++) = datum;
+                datum = datum * 4294967311ULL + 17;
+            }
+
+            pointer = (char *)buffer;
+            size = pagesize;
+            while (size > 0) {
+                rc = write(fd, pointer, size);
+                if (rc < 0) {
+                    perror("write");
+                    break;
+                } else if (rc == 0) {
+                    fprintf(stderr, "write: eof\n");
+                    break;
+                } else {
+                    total += rc; 
+                    pointer += rc;
+                    size -= rc;
+                }
+            }
+
+        } while (rc > 0);
+
+        end = time(0);
+
+    	fprintf(stderr, "%s: %llu bytes written\n", argv[0], total);
+    	fprintf(stderr, "%s: %lu seconds elapsed\n", argv[0], end - start);
+
+        fd = close(fd);
+        if (fd < 0) {
+            perror("close");
+            break;
+        }
+
+        fd = open(argv[1], O_RDONLY | O_DIRECT, 0);
+        if (fd < 0) {
+            perror(argv[1]);
+            break;
+        }
+
+        datum = base;
+        total = 0;
+        start = time(0);
+
+        do {
+
+            pointer = (char *)buffer;
+            size = pagesize;
+            while (size > 0) {
+                rc = read(fd, pointer, size);
+                if (rc < 0) {
+                    perror("read");
+                    break;
+                } else if (rc == 0) {
+                    fprintf(stderr, "read: eof\n");
+                    break;
+                } else {
+                    total += rc; 
+                    pointer += rc;
+                    size -= rc;
+                }
+            }
+
+            if (rc <= 0) {
+                break;
+            }
+
+            error = 0;
+            data = (uint64_t *)buffer;
+            size = pagesize / sizeof(datum);
+            while ((size--) > 0) {
+                if (*(data++) != datum) {
+                    ++errors;
+                    error = !0;
+                }
+                datum = datum * 4294967311ULL + 17;
+            }
+            if (error) {
+                end = time(0);
+    	        fprintf(stderr, "%s: %llu errors %llu bytes %lu seconds\n", argv[0], errors, total, end - start);
+            }
+
+        } while (rc > 0);
+
+        end = time(0);
+
+    	fprintf(stderr, "%s: %llu bytes read\n", argv[0], total);
+    	fprintf(stderr, "%s: %lu seconds elapsed\n", argv[0], end - start);
+    	fprintf(stderr, "%s: %llu errors\n", argv[0], errors);
+
+        fd = close(fd);
+        if (fd < 0) {
+            perror("close");
+            break;
+        }
+
+        fd = -1;
+
     } while (0);
 
-    if (fd >= 0) {
-        close(fd);
+    if (fd < 0) {
+        /* Do nothing. */
+    } else if ((fd = close(fd))) {
+        perror("close");
+    } else {
+        /* Do nothing. */
     }
 
-    return 0;
+    return xc;
 }
