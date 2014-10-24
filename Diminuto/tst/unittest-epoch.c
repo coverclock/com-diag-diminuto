cf121072496d12868831b866b0b979db14eba74b
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2014-10-24 21:55:44 +0000

    Added some more interesting unit tests to epoch. See http://en.wikipedia.org/wiki/Unix_time.
    git-svn-id: svn://graphite/diminuto/trunk@525 5dfbf39a-1452-0410-8a97-9e58fedde8e3

--- Diminuto/tst/unittest-epoch.c
+++ Diminuto/tst/unittest-epoch.c
@@ -19,19 +19,19 @@
 
 static void epoch(diminuto_ticks_t now, int verbose)
 {
-	int dday = -1;
-	int dhour = -1;
-	int dminute = -1;
-	int dsecond = -1;
-	int dtick = -1;
-	int zyear = -1;
+    int dday = -1;
+    int dhour = -1;
+    int dminute = -1;
+    int dsecond = -1;
+    int dtick = -1;
+    int zyear = -1;
     int zmonth = -1;
     int zday = -1;
     int zhour = -1;
     int zminute = -1;
     int zsecond = -1;
     int ztick = -1;
-	int jyear = -1;
+    int jyear = -1;
     int jmonth = -1;
     int jday = -1;
     int jhour = -1;
@@ -63,37 +63,37 @@ static void epoch(diminuto_ticks_t now, int verbose)
     dm = (daylightsaving / hertz) % 3600;
     rc = diminuto_time_duration(now, &dday, &dhour, &dminute, &dsecond, &dtick);
     if (rc < 0) { dday = -dday; }
-	if ((now != zulu) || (now != juliet) || verbose || (zyear != prior)) {
-    	DIMINUTO_LOG_DEBUG("%20lld %20lld %20lld %4.4d-%2.2d-%2.2dT%2.2d:%2.2d:%2.2d.%9.9d-%2.2d:%2.2d+%2.2d:%2.2d %4.4d-%2.2d-%2.2dT%2.2d:%2.2d:%2.2d.%9.9d-%2.2d:%2.2d+%2.2d:%2.2d %6d/%2.2d:%2.2d:%2.2d.%9.9d\n"
-    		, now, zulu, juliet
-    		, zyear, zmonth, zday, zhour, zminute, zsecond, ztick, 0, 0, 0, 0
-    		, jyear, jmonth, jday, jhour, jminute, jsecond, jtick, zh, zm, dh, dm
-    		, dday, dhour, dminute, dsecond, dtick
-    	);
-	}
-	ASSERT(now == zulu);
-	ASSERT(now == juliet);
-	ASSERT((1901 <= zyear) && (zyear <= 2038));
-	ASSERT((1 <= zmonth) && (zmonth <= 12));
-	ASSERT((1 <= zday) && (zday <= 31));
-	ASSERT((0 <= zhour) && (zhour <= 23));
-	ASSERT((0 <= zminute) && (zminute <= 59));
-	ASSERT((0 <= zsecond) && (zsecond <= 60)); /* To account for leap seconds. */
-	ASSERT((0 <= ztick) && (ztick <= 999999));
-	ASSERT((1901 <= jyear) && (jyear <= 2038));
-	ASSERT((1 <= jmonth) && (jmonth <= 12));
-	ASSERT((1 <= jday) && (jday <= 31));
-	ASSERT((0 <= jhour) && (jhour <= 23));
-	ASSERT((0 <= jminute) && (jminute <= 59));
-	ASSERT((0 <= jsecond) && (jsecond <= 60)); /* To account for leap seconds. */
-	ASSERT((0 <= jtick) && (jtick <= 999999));
-	ASSERT((0 <= dhour) && (dhour <= 23));
-	ASSERT((0 <= dminute) && (dminute <= 59));
-	ASSERT((0 <= dsecond) && (dsecond <= 60)); /* To account for leap seconds. */
-	ASSERT((0 <= dtick) && (dtick <= 999999));
-	ASSERT((rc < 0) || (rc > 0));
+    if ((now != zulu) || (now != juliet) || verbose || (zyear != prior)) {
+        DIMINUTO_LOG_DEBUG("%20lld %20lld %20lld %4.4d-%2.2d-%2.2dT%2.2d:%2.2d:%2.2d.%9.9d-%2.2d:%2.2d+%2.2d:%2.2d %4.4d-%2.2d-%2.2dT%2.2d:%2.2d:%2.2d.%9.9d-%2.2d:%2.2d+%2.2d:%2.2d %6d/%2.2d:%2.2d:%2.2d.%9.9d\n"
+            , now, zulu, juliet
+            , zyear, zmonth, zday, zhour, zminute, zsecond, ztick, 0, 0, 0, 0
+            , jyear, jmonth, jday, jhour, jminute, jsecond, jtick, zh, zm, dh, dm
+            , dday, dhour, dminute, dsecond, dtick
+        );
+    }
+    ASSERT(now == zulu);
+    ASSERT(now == juliet);
+    ASSERT((1901 <= zyear) && (zyear <= 2038));
+    ASSERT((1 <= zmonth) && (zmonth <= 12));
+    ASSERT((1 <= zday) && (zday <= 31));
+    ASSERT((0 <= zhour) && (zhour <= 23));
+    ASSERT((0 <= zminute) && (zminute <= 59));
+    ASSERT((0 <= zsecond) && (zsecond <= 60)); /* To account for leap seconds. */
+    ASSERT((0 <= ztick) && (ztick <= 999999));
+    ASSERT((1901 <= jyear) && (jyear <= 2038));
+    ASSERT((1 <= jmonth) && (jmonth <= 12));
+    ASSERT((1 <= jday) && (jday <= 31));
+    ASSERT((0 <= jhour) && (jhour <= 23));
+    ASSERT((0 <= jminute) && (jminute <= 59));
+    ASSERT((0 <= jsecond) && (jsecond <= 60)); /* To account for leap seconds. */
+    ASSERT((0 <= jtick) && (jtick <= 999999));
+    ASSERT((0 <= dhour) && (dhour <= 23));
+    ASSERT((0 <= dminute) && (dminute <= 59));
+    ASSERT((0 <= dsecond) && (dsecond <= 60)); /* To account for leap seconds. */
+    ASSERT((0 <= dtick) && (dtick <= 999999));
+    ASSERT((rc < 0) || (rc > 0));
 
-	prior = zyear;
+    prior = zyear;
 }
 
 static const diminuto_ticks_t LOW = 0xffffffff80000000LL;
@@ -110,6 +110,16 @@ int main(int argc, char ** argv)
 
     hertz = diminuto_time_frequency();
 
+    DIMINUTO_LOG_INFORMATION("TEST 0\n");
+
+    epoch(0xffffffff80000000LL * hertz, !0);
+    epoch(0, !0);
+    epoch(1000000000LL * hertz, !0);
+    epoch(1234567890LL * hertz, !0);
+    epoch(15000LL * 24LL * 3600LL * hertz, !0);
+    epoch(1400000000LL * hertz, !0);
+    epoch(0x000000007fffffffLL * hertz, !0);
+
     DIMINUTO_LOG_INFORMATION("TEST 1\n");
 
     epoch(LOW * hertz, !0);
@@ -120,31 +130,31 @@ int main(int argc, char ** argv)
     DIMINUTO_LOG_INFORMATION("TEST 2\n");
 
     for (now = LOW; now <= HIGH; now += (365 * 24 * 60 * 60)) {
-    	epoch(now * hertz, 0);
+        epoch(now * hertz, 0);
      }
 
     DIMINUTO_LOG_INFORMATION("TEST 3\n");
 
     for (now = LOW; now <= HIGH; now += (24 * 60 * 60)) {
-    	epoch(now * hertz, 0);
+        epoch(now * hertz, 0);
     }
 
     DIMINUTO_LOG_INFORMATION("TEST 4\n");
 
     for (now = LOW; now <= HIGH; now += (60 * 60)) {
-    	epoch(now * hertz, 0);
+        epoch(now * hertz, 0);
     }
 
     DIMINUTO_LOG_INFORMATION("TEST 5\n");
 
     for (now = LOW; now <= HIGH; now += 60) {
-    	epoch(now * hertz, 0);
+        epoch(now * hertz, 0);
     }
 
     DIMINUTO_LOG_INFORMATION("TEST 6\n");
 
     for (now = LOW; now <= HIGH; now += 1) {
-    	epoch(now * hertz, 0);
+        epoch(now * hertz, 0);
     }
 
     EXIT();
