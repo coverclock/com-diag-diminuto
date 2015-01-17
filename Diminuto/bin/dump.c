bab30b184d6c42480786e92dff627baf55bf1578
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2015-01-17 17:36:22 +0000

    dump utility now takes file names on command line. Bumped version.
    git-svn-id: svn://graphite/diminuto/trunk@620 5dfbf39a-1452-0410-8a97-9e58fedde8e3

--- Diminuto/bin/dump.c
+++ Diminuto/bin/dump.c
@@ -2,30 +2,33 @@
 /**
  * @file
  *
- * Copyright 2010 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Copyright 2010, 2015 Digital Aggregates Corporation, Colorado, USA<BR>
  * Licensed under the terms in README.h<BR>
  * Chip Overclock <coverclock@diag.com><BR>
  * http://www.diag.com/navigation/downloads/Diminuto.html<BR>
  *
  * USAGE
  *
- * dump [ -t ]
+ * dump [ -t ] [ [ file | - ] ... ]
  *
  * EXAMPLES
  *
+ * dump binaryfile1 binaryfile2 binaryfile3
+ * dump binaryfile1 - binaryfile3 < binaryfile2
  * dump < binaryfile
  * cat binaryfile | dump
  * cat binaryfile | dump -t | someothercommand
  *
  * ABSTRACT
  *
- * Dumps stdin to stdout or to stderr while copying stdin to stdout.
+ * Dumps files or stdin to stdout, or to stderr while copying input to stdout.
  */
 
 #include "com/diag/diminuto/diminuto_dump.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
 static const char * program = "dump";
 
@@ -36,14 +39,26 @@ static void usage(FILE * fp)
     fprintf(fp, "       -?          Print menu\n");
 }
 
-int main(int argc, char * argv[])
+static void dump(FILE * in, FILE * out)
 {
-    FILE * out = stdout;
     uintptr_t offset = 0;
-    int opt;
-    extern char * optarg;
     uint8_t buffer[16];
     size_t length;
+
+    while ((length = fread(buffer, 1, sizeof(buffer), in)) > 0) {
+        diminuto_dump_general(out, buffer, length, 0, '.', 1, offset, 0);
+        if (out == stderr) { fwrite(buffer, length, 1, stdout); }
+        offset += length;
+    }
+}
+
+int main(int argc, char * argv[])
+{
+    FILE * in = stdin;
+    FILE * out = stdout;
+    const char * name = "-";
+    int opt;
+    extern char * optarg;
 
     program = strrchr(argv[0], '/');
     program = (program == (char *)0) ? argv[0] : program + 1;
@@ -58,11 +73,6 @@ int main(int argc, char * argv[])
 
         case '?':
             usage(stderr);
-            return 0;
-            break;
-
-        default:
-            usage(stderr);
             return 1;
             break;
 
@@ -70,10 +80,38 @@ int main(int argc, char * argv[])
 
     }
 
-    while ((length = fread(buffer, 1, sizeof(buffer), stdin)) > 0) {
-        diminuto_dump_general(out, buffer, length, 0, '.', 1, offset, 0);
-        if (out == stderr) { fwrite(buffer, length, 1, stdout); }
-        offset += length;
+    if (optind < argc) {
+        for (; optind < argc; ++optind) {
+            name = argv[optind];
+            if (strcmp(name, "-") == 0) {
+                in = stdin;
+            } else if ((in = fopen(name, "rb")) == (FILE *)0) {
+                perror(name);
+                return 2;
+            } else {
+                /* Do nothing. */
+            }
+            dump(in, out);
+            if (ferror(in)) {
+                errno = EIO;
+                perror(name);
+                return 3;
+            }
+            if (in == stdin) {
+                /* Do nothing. */
+            } else if (fclose(in) == 0) {
+                /* Do nothing. */
+            } else {
+                perror(name);
+            }
+        }
+    } else {
+        dump(in, out);
+        if (ferror(in)) {
+            errno = EIO;
+            perror(name);
+            return 3;
+        }
     }
 
     return 0;
