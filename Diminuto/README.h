7d89fb87bd114ff01d5689dc9de7c152f4397ca2
jsloan <jsloan@5dfbf39a-1452-0410-8a97-9e58fedde8e3>
2015-01-21 23:12:23 +0000

    Updated copyrights.
    git-svn-id: svn://graphite/diminuto/trunk@623 5dfbf39a-1452-0410-8a97-9e58fedde8e3

--- Diminuto/README.h
+++ Diminuto/README.h
@@ -4,7 +4,7 @@
 
 /*******************************************************************************
 
-    Copyright 2008-2014 by the Digital Aggregates Corporation, Colorado, USA.
+    Copyright 2008-2015 by the Digital Aggregates Corporation, Colorado, USA.
 
     ----------------------------------------------------------------------
 
