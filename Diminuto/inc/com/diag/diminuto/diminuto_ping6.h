c4a92a2458acb3c822fe76bdc0b30e34ee0e7d1c
Chip Coverclock <coverclock@diag.com>
2016-12-09 10:23:52 -0700

    [1] Comments.

--- Diminuto/inc/com/diag/diminuto/diminuto_ping6.h
+++ Diminuto/inc/com/diag/diminuto/diminuto_ping6.h
@@ -1,11 +1,11 @@
 /* vi: set ts=4 expandtab shiftwidth=4: */
-#ifndef _H_COM_DIAG_DIMINUTO_PING_
-#define _H_COM_DIAG_DIMINUTO_PING_
+#ifndef _H_COM_DIAG_DIMINUTO_PING6_
+#define _H_COM_DIAG_DIMINUTO_PING6_
 
 /**
  * @file
  *
- * Copyright 2015 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Copyright 2015-2016 Digital Aggregates Corporation, Colorado, USA<BR>
  * Licensed under the terms in README.h<BR>
  * Chip Overclock <coverclock@diag.com><BR>
  * http://www.diag.com/navigation/downloads/Diminuto.html<BR>
