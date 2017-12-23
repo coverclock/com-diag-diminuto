1880a5046fcf3310de67cf5f0f5b4dfab77c75d2
Chip Overclock <coverclock@diag.com>
2017-12-22 19:12:29 -0700

    [1] Expanded critical section unit test. [2] Unit test passes.

--- Diminuto/tst/unittest-criticalsection.c
+++ Diminuto/tst/unittest-criticalsection.c
@@ -13,25 +13,39 @@
 #include <pthread.h>
 #include <errno.h>
 
-/*
- * This is mostly just to insure it compiles.
- */
-
 int main(void)
 {
-	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+	pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+	pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
 
-	ASSERT(pthread_mutex_trylock(&mutex) == 0);
-	pthread_mutex_unlock(&mutex);
+	ASSERT(pthread_mutex_trylock(&mutex1) == 0);
+	pthread_mutex_unlock(&mutex1);
+	ASSERT(pthread_mutex_trylock(&mutex2) == 0);
+	pthread_mutex_unlock(&mutex2);
 
-	DIMINUTO_CRITICAL_SECTION_BEGIN(&mutex);
+	DIMINUTO_CRITICAL_SECTION_BEGIN(&mutex1);
 
-		ASSERT(pthread_mutex_trylock(&mutex) == EBUSY);
+		ASSERT(pthread_mutex_trylock(&mutex1) == EBUSY);
+		ASSERT(pthread_mutex_trylock(&mutex2) == 0);
+		pthread_mutex_unlock(&mutex2);
+
+		DIMINUTO_CRITICAL_SECTION_BEGIN(&mutex2);
+
+			ASSERT(pthread_mutex_trylock(&mutex1) == EBUSY);
+			ASSERT(pthread_mutex_trylock(&mutex2) == EBUSY);
+
+		DIMINUTO_CRITICAL_SECTION_END;
+
+		ASSERT(pthread_mutex_trylock(&mutex1) == EBUSY);
+		ASSERT(pthread_mutex_trylock(&mutex2) == 0);
+		pthread_mutex_unlock(&mutex2);
 
 	DIMINUTO_CRITICAL_SECTION_END;
 
-	ASSERT(pthread_mutex_trylock(&mutex) == 0);
-	pthread_mutex_unlock(&mutex);
+	ASSERT(pthread_mutex_trylock(&mutex1) == 0);
+	pthread_mutex_unlock(&mutex1);
+	ASSERT(pthread_mutex_trylock(&mutex2) == 0);
+	pthread_mutex_unlock(&mutex2);
 
     EXIT();
 }
