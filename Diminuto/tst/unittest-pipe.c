376e3bb623d3cbb76b17995803eaaedaec486a5c
Chip Overclock <coverclock@diag.com>
2018-01-31 10:28:29 -0700

    [1] Removed surpise from pipe unit test. [2] Unit test passes.

--- Diminuto/tst/unittest-pipe.c
+++ Diminuto/tst/unittest-pipe.c
@@ -23,11 +23,11 @@
 
 extern int diminuto_pipe_debug;
 
+static const char BUFFER[] = "unittest-pipe | cat > /dev/null & sleep 1 & pkill cat\n";
+
 int main(int argc, char ** argv)
 {
-    int fd = 0;
     int rc = 0;
-    uint8_t buffer[512];
 
     SETLOGMASK();
 
@@ -41,24 +41,15 @@ int main(int argc, char ** argv)
     rc = diminuto_pipe_check();
     ASSERT(rc == 0);
 
-    fd = open("/dev/urandom", O_RDONLY);
-    ASSERT(fd >= 0);
-
     while (!0) {
 
-        rc = read(fd, buffer, sizeof(buffer));
-        if (rc <= 0) { break; }
-
-        (void)write(STDOUT_FILENO, buffer, rc);
+        (void)write(STDOUT_FILENO, BUFFER, sizeof(BUFFER));
 
         rc = diminuto_pipe_check();
         ASSERT(rc >= 0);
         if (rc > 0) { break; }
 
 	}
-
-    rc = close(fd);
-    ASSERT(rc >= 0);
 
     rc = diminuto_pipe_check();
     ASSERT(rc == 0);
