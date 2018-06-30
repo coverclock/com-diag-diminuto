0984d21076cb54d593c356761cc220162624741c
Chip Overclock <coverclock@diag.com>
2018-06-30 03:03:55 +0000

    [1] Checkpoint.

--- Diminuto/bin/pidtest.c
+++ Diminuto/bin/pidtest.c
@@ -7,23 +7,24 @@
  * Chip Overclock <coverclock@diag.com><BR>
  * https://github.com/coverclock/com-diag-diminuto<BR>
  *
- * USAGE
- *
- * EXAMPLES
- *
- * ABSTRACT
- *
  * REFERENCES
  *
+ * Avago, "APDS-9301 Miniature Ambient Light Photo Sensor with Digital
+ * (I2C) Output", Avago Technologies, AV02-2315EN, 2010-01-07
+ *
+ * Wikipedia, "Lux", 2018-03-17
  */
 
 #include "com/diag/diminuto/diminuto_controller.h"
 #include "com/diag/diminuto/diminuto_countof.h"
 #include "com/diag/diminuto/diminuto_criticalsection.h"
+#include "com/diag/diminuto/diminuto_delay.h"
+#include "com/diag/diminuto/diminuto_frequency.h"
 #include "com/diag/diminuto/diminuto_interrupter.h"
 #include "com/diag/diminuto/diminuto_i2c.h"
 #include "com/diag/diminuto/diminuto_log.h"
 #include "com/diag/diminuto/diminuto_modulator.h"
+#include "com/diag/diminuto/diminuto_mux.h"
 #include "com/diag/diminuto/diminuto_pin.h"
 #include "com/diag/diminuto/diminuto_terminator.h"
 
@@ -72,13 +73,22 @@ int main(int argc, char ** argv) {
     int fd = -1;
     int rc = -1;
     uint8_t datum = 0;
+    int led = 12;
     int bus = 1;
     int device = 0x39;
+    int interrupt = 26;
+    FILE * fp = (FILE *)0;
     uint16_t chan0 = 0;
     uint16_t chan1 = 0;
     double lux = 0.0;
+    diminuto_mux_t mux;
+    diminuto_ticks_t ticks = 0;
 
     program = ((program = strrchr(argv[0], '/')) == (char *)0) ? argv[0] : program + 1;
+
+    /*
+     * I2C light sensor.
+     */
 
     fd = diminuto_i2c_open(bus);
     assert(fd >= 0);
@@ -110,25 +120,102 @@ int main(int argc, char ** argv) {
         }
     }
 
-    rc = diminuto_i2c_get(fd, device, 0xcc, &datum);
+    /*
+     * GPIO interrupt pin.
+     */
+
+    (void)diminuto_pin_unexport_ignore(interrupt); 
+
+    fp = diminuto_pin_input(interrupt);
+    assert(fp != (FILE *)0);
+
+    rc = diminuto_pin_edge(interrupt, DIMINUTO_PIN_EDGE_FALLING);
+    assert(rc == 0);
+
+    /*
+     * Multiplexer.
+     */
+
+    diminuto_mux_init(&mux);
+
+    rc = diminuto_mux_register_interrupt(&mux, fileno(fp));
     assert(rc >= 0);
-    chan0 = datum;
 
-    rc = diminuto_i2c_get(fd, device, 0x8d, &datum);
+    /*
+     * Signal handlers.
+     */
+
+    rc = diminuto_terminator_install(!0);
     assert(rc >= 0);
-    chan0 = ((uint16_t)datum << 8) | chan0;
 
-    rc = diminuto_i2c_get(fd, device, 0x8e, &datum);
+    rc = diminuto_interrupter_install(!0);
     assert(rc >= 0);
-    chan1 = datum;
 
-    rc = diminuto_i2c_get(fd, device, 0x8f, &datum);
-    assert(rc >= 0);
-    chan1 = ((uint16_t)datum << 8) | chan1;
+    /*
+     * Work loop.
+     */
 
-    lux = apds_9310_chan2lux(chan0, chan1);
+    ticks = diminuto_frequency() / 2;
 
-    printf("%s: 0x%04x 0x%04x %.2f\n", program, chan0, chan1, lux);
+    while (!0) {
+
+        rc = diminuto_mux_wait(&mux, -1);
+
+        if (rc >= 0) {
+            /* Do nothing. */
+        } else if (errno != EINTR) {
+            break;
+        } else if (diminuto_terminator_check()) {
+            break;
+        } else if (diminuto_interrupter_check()) {
+            break;
+        } else {
+            diminuto_delay(ticks, !0);
+            continue;
+        }
+
+        if (rc == 0) {
+            diminuto_delay(ticks, !0);
+            continue;
+        }
+
+        if (diminuto_mux_ready_interrupt(&mux) != fileno(fp)) {
+            diminuto_delay(ticks, !0);
+            continue;
+        }
+
+        rc = diminuto_i2c_get(fd, device, 0xcc, &datum);
+        assert(rc >= 0);
+        chan0 = datum;
+
+        rc = diminuto_i2c_get(fd, device, 0x8d, &datum);
+        assert(rc >= 0);
+        chan0 = ((uint16_t)datum << 8) | chan0;
+
+        rc = diminuto_i2c_get(fd, device, 0x8e, &datum);
+        assert(rc >= 0);
+        chan1 = datum;
+
+        rc = diminuto_i2c_get(fd, device, 0x8f, &datum);
+        assert(rc >= 0);
+        chan1 = ((uint16_t)datum << 8) | chan1;
+
+        lux = apds_9310_chan2lux(chan0, chan1);
+
+        printf("%s: 0x%04x 0x%04x %.2f\n", program, chan0, chan1, lux);
+
+    }
+
+    /*
+     * Done.
+     */
+
+    diminuto_mux_unregister_interrupt(&mux, fileno(fp));
+
+    diminuto_mux_fini(&mux);
+
+    fp = diminuto_pin_unused(fp, interrupt);
+    assert(fp == (FILE *)0);
 
     fd = diminuto_i2c_close(fd);
     assert(fd < 0);
