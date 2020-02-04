482188cba602d97c644eb9afc1313e0a72b4bfa6
Chip Overclock <coverclock@diag.com>
2020-02-04 11:40:56 -0700

    [1] Fixed memory leak in walker. [2] Unit tests PASS.

--- Diminuto/src/diminuto_fs.c
+++ Diminuto/src/diminuto_fs.c
@@ -68,6 +68,7 @@ diminuto_fs_type_t diminuto_fs_type(mode_t mode)
 
 int diminuto_fs_walker(const char * name, char * path, size_t total, size_t depth, diminuto_fs_walker_t * walkerp, void * statep)
 {
+    int xc = 0;
     DIR * dp = (DIR *)0;
     struct dirent * ep = (struct dirent *)0;
     struct stat status = { 0 };
@@ -76,147 +77,168 @@ int diminuto_fs_walker(const char * name, char * path, size_t total, size_t dept
     size_t prior = 0;
     size_t length = 0;
 
-    /*
-     * Limit the depth of recursion to something reasonable.
-     * This is still pretty big. The check below will limit
-     * it as well. This is just a safety valve.
-     */
+    do {
 
-    if (depth > (PATH_MAX / 2)) {
-        errno = E2BIG;
-        diminuto_perror(path);
-        return -3;
-    }
+        /*
+         * Limit the depth of recursion to something reasonable.
+         * This is still pretty big. The check below will limit
+         * it as well. This is just a safety valve.
+         */
 
-    /*
-     * Insure the path buffer has sufficient room. I'd be surprised
-     * if this failed on a modern system, but back when MAXPATHLEN
-     * was 512 I have seen file systems for which an absolute path
-     * string could not be represented.
-     */
-
-    length = strnlen(name, PATH_MAX);
-    if (length == 0) {
-        errno = EINVAL;
-        diminuto_perror(path);
-        return -4;
-    } else if ((total + 1 /* '/' */ + length + 1 /* '\0' */) > PATH_MAX) {
-        errno = EFBIG;
-        diminuto_perror(path);
-        return -5;
-    } else {
-        /* Do nothing. */
-    }
-
-    if (Debug) { fprintf(stderr, "%s@%d: \"%s\" [%zu] \"%s\" [%zu]\n", __FILE__, __LINE__, path, total, name, length); }
-
-    /*
-     * Contstruct a path (maybe be relative or absolute depending
-     * on the root).
-     */
-
-    prior = total;
-    if (total == 0) {
-        /* Do nothing. */
-    } else if (path[total - 1] == '/') {
-        /* Do nothing. */
-    } else {
-        path[total++] = '/';
-        path[total] = '\0';
-    }
-    strcat(path, name);
-    total += length;
-
-    if (Debug) { fprintf(stderr, "%s@%d: \"%s\" [%zu]\n", __FILE__, __LINE__, path, total); }
-
-    /*
-     * Get the attributes for the file identified by the path.
-     */
-
-    rc = lstat(path, &status);
-    if (rc >= 0) {
-        /* Do nothing. */
-    } else if ((errno == EACCES) || (errno == ENOENT)) {
-#if 0
-        diminuto_perror(path);
-#endif
-        path[prior] = '\0';
-        return 0;
-    } else {
-        diminuto_perror(path);
-        return -6;
-    }
-
-    /*
-     * Invoke the callback.
-     */
-
-    if (walkerp == (diminuto_fs_walker_t *)0) {
-        /* Do nothing. */
-    } else if ((rc = (*walkerp)(statep, name, path, depth, &status)) == 0) {
-        /* Do nothing. */
-    } else {
-        return rc;
-    }
-
-    /*
-     * If a flat file, we're done; if a directory, recurse and descend.
-     */
-
-    if (S_ISDIR(status.st_mode)) {
-
-        dp = opendir(path);
-        if (dp != (DIR *)0) {
-            /* Do nothing. */
-        } else if ((errno == EACCES) || (errno == ENOENT)) {
-#if 0
+        if (depth > (PATH_MAX / 2)) {
+            errno = E2BIG;
             diminuto_perror(path);
-#endif
-            path[prior] = '\0';
-            return 0;
-        } else {
-            diminuto_perror(path);
-            return -7;
+            xc = -3;
+            break;
         }
 
-        depth += 1;
+        /*
+         * Insure the path buffer has sufficient room. I'd be surprised
+         * if this failed on a modern system, but back when MAXPATHLEN
+         * was 512 I have seen file systems for which an absolute path
+         * string could not be represented.
+         */
 
-        while (!0) {
+        length = strnlen(name, PATH_MAX);
+        if (length == 0) {
+            errno = EINVAL;
+            diminuto_perror(path);
+            xc = -4;
+            break;
+        } else if ((total + 1 /* '/' */ + length + 1 /* '\0' */) > PATH_MAX) {
+            errno = EFBIG;
+            diminuto_perror(path);
+            xc = -5;
+            break;
+        } else {
+            /* Do nothing. */
+        }
 
-            errno = 0;
-            if ((ep = readdir(dp)) != (struct dirent *)0) {
+        if (Debug) { fprintf(stderr, "%s@%d: \"%s\" [%zu] \"%s\" [%zu]\n", __FILE__, __LINE__, path, total, name, length); }
+
+        /*
+         * Contstruct a path (maybe be relative or absolute depending
+         * on the root).
+         */
+
+        prior = total;
+        if (total == 0) {
+            /* Do nothing. */
+        } else if (path[total - 1] == '/') {
+            /* Do nothing. */
+        } else {
+            path[total++] = '/';
+            path[total] = '\0';
+        }
+        strcat(path, name);
+        total += length;
+
+        if (Debug) { fprintf(stderr, "%s@%d: \"%s\" [%zu]\n", __FILE__, __LINE__, path, total); }
+
+        /*
+         * Get the attributes for the file identified by the path.
+         */
+
+        rc = lstat(path, &status);
+        if (rc >= 0) {
+            /* Do nothing. */
+        } else if ((errno == EACCES) || (errno == ENOENT)) {
+            if (Debug) {
+                diminuto_perror(path);
+            }
+            path[prior] = '\0';
+            break;
+        } else {
+            diminuto_perror(path);
+            xc = -6;
+            break;
+        }
+
+        /*
+         * Invoke the callback.
+         */
+
+        if (walkerp == (diminuto_fs_walker_t *)0) {
+            /* Do nothing. */
+        } else if ((rc = (*walkerp)(statep, name, path, depth, &status)) == 0) {
+            /* Do nothing. */
+        } else {
+            xc = rc;
+            break;
+        }
+
+        /*
+         * If a flat file, we're done; if a directory, recurse and descend.
+         */
+
+        if (S_ISDIR(status.st_mode)) {
+
+            dp = opendir(path);
+            if (dp != (DIR *)0) {
                 /* Do nothing. */
-            } else if (errno == 0) {
+            } else if ((errno == EACCES) || (errno == ENOENT)) {
+                if (Debug) {
+                    diminuto_perror(path);
+                }
+                path[prior] = '\0';
                 break;
             } else {
                 diminuto_perror(path);
-                return -8;
+                xc = -7;
+                break;
             }
 
-            if (strcmp(ep->d_name, "..") == 0) {
-                /* Do ntohing. */
-            } else if (strcmp(ep->d_name, ".") == 0) {
-                /* Do ntohing. */
-            } else if ((rc = diminuto_fs_walker(ep->d_name, path, total, depth, walkerp, statep)) == 0) {
-                /* Do ntohing. */
-            } else {
-                return rc;
+            depth += 1;
+
+            while (!0) {
+
+                errno = 0;
+                if ((ep = readdir(dp)) != (struct dirent *)0) {
+                    /* Do nothing. */
+                } else if (errno == 0) {
+                    break;
+                } else {
+                    diminuto_perror(path);
+                    xc = -8;
+                    break;
+                }
+
+                /*
+                 * See readdir(3): the memory returned by the function
+                 * is not dynamically allocated.
+                 */
+
+                if (strcmp(ep->d_name, "..") == 0) {
+                    /* Do ntohing. */
+                } else if (strcmp(ep->d_name, ".") == 0) {
+                    /* Do ntohing. */
+                } else if ((rc = diminuto_fs_walker(ep->d_name, path, total, depth, walkerp, statep)) == 0) {
+                    /* Do ntohing. */
+                } else {
+                    xc = rc;
+                    break;
+                }
+
             }
 
         }
 
-        if (closedir(dp) < 0) {
-            diminuto_perror(path);
-            return -9;
-        }
+        path[prior] = '\0';
 
+        if (Debug) { fprintf(stderr, "%s@%d: \"%s\" [%zu]\n", __FILE__, __LINE__, path, prior); }
+
+    } while (0);
+
+    if (dp == (DIR *)0) {
+        /* Do nothing. */
+    } else if (closedir(dp) < 0) {
+        diminuto_perror(path);
+        xc = -9;
+    } else {
+        dp = (DIR *)0;
     }
 
-    path[prior] = '\0';
-
-    if (Debug) { fprintf(stderr, "%s@%d: \"%s\" [%zu]\n", __FILE__, __LINE__, path, prior); }
-
-    return 0;
+    return xc;
 }
 
 int diminuto_fs_walk(const char * root, diminuto_fs_walker_t * walkerp, void * statep)
