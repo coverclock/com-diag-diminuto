87457ca131a78948c94b7be796084e17e3c511bb
Chip Overclock <coverclock@diag.com>
2020-06-19 12:34:09 -0600

    [1] Minor tweak of assert output. TESTED

--- Diminuto/src/diminuto_assert.c
+++ Diminuto/src/diminuto_assert.c
@@ -15,7 +15,7 @@
 void diminuto_assert_f(int cond, const char * string, const char * file, int line, int error, int code)
 {
     if (!cond) {
-        diminuto_log_log(DIMINUTO_LOG_PRIORITY_ERROR, "%s[%d]: FAIL! (%s) <%d> \"%s\" {%d}\n", file, line, string, error, error ? strerror(error) : "", code);
+        diminuto_log_log(DIMINUTO_LOG_PRIORITY_ERROR, "%s[%d]: ASSERT (%s) FAILED! %d=\"%s\" %d\n", file, line, string, error, error ? strerror(error) : "", code);
         if (code) {
             exit(code);
         }
