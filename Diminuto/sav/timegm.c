dd541f7b5b57600b668de9bca80866b949e0a8b0
Chip Overclock <coverclock@diag.com>
2020-10-19 13:38:54 -0600

    [1] Expanded timegm experiment.

--- Diminuto/sav/timegm.c
+++ Diminuto/sav/timegm.c
@@ -3,18 +3,25 @@
 #include <errno.h>
 
 void main(void) {
-    time_t seconds;
-    struct tm datetime;
+    struct tm earlier = { 0, };
+    time_t seconds = 0;
+    struct tm later = { 0, };;
+    struct tm * pointer = (struct tm *)0;
 
-    datetime.tm_year = 1901 - 1900;
-    datetime.tm_mon = 12 - 1;
-    datetime.tm_mday = 13;
-    datetime.tm_hour = 13;
-    datetime.tm_min = 45;
-    datetime.tm_sec = 52;
-    datetime.tm_isdst = 0;
+    earlier.tm_year = 1901 - 1900;
+    earlier.tm_mon = 12 - 1;
+    earlier.tm_mday = 13;
+    earlier.tm_hour = 13;
+    earlier.tm_min = 45;
+    earlier.tm_sec = 52;
+    earlier.tm_isdst = 0;
+    printf("earlier=%d/%d/%dT%d:%d:%d+%d\n", earlier.tm_year, earlier.tm_mon, earlier.tm_mday, earlier.tm_hour, earlier.tm_min, earlier.tm_sec, earlier.tm_isdst);
 
-    seconds = timegm(&datetime);
-    printf("timegm=%ld=0x%lx\n", seconds, seconds);
+    seconds = timegm(&earlier);
     if (seconds == (time_t)-1) { perror("timegm"); }
+    printf("seconds=%ld=0x%lx\n", seconds, seconds);
+
+    pointer = localtime_r(&seconds, &later);
+    if (pointer == (struct tm *)0) { perror("localtime_r"); }
+    printf("later=%d/%d/%dT%d:%d:%d+%d\n", later.tm_year, later.tm_mon, later.tm_mday, later.tm_hour, later.tm_min, later.tm_sec, later.tm_isdst);
 }
