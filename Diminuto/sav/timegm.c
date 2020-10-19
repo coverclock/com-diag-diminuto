6043147f5ad8e80dd80abca6ac3527de075111e3
Chip Overclock <coverclock@diag.com>
2020-10-19 16:16:43 -0600

    [1] Getting ready to move to fun.

--- Diminuto/sav/timegm.c
+++ Diminuto/sav/timegm.c
@@ -4,82 +4,80 @@
 #include <stdint.h>
 
 void main(void) {
-    int64_t problematic = 0;
     struct tm zulu = { 0, };
     struct tm juliet = { 0, };
-    struct tm earlier = { 0, };
-    time_t seconds = 0;
-    struct tm later = { 0, };;
+    struct tm data = { 0, };
     struct tm * pointer = (struct tm *)0;
+    time_t seconds = (time_t)0;
 
     printf("sizeof=%zu\n", sizeof(time_t));
 
-    problematic = 0xffffffff80000000LL;
-    printf("problematic=%lld=0x%llx\n", (long long)problematic, (long long)problematic);
+    seconds = (time_t)0xffffffff80000000LL;
+    printf("seconds=%ld=0x%08lx\n", (long)seconds, (long)seconds);
 
-    seconds = problematic;
-    pointer = localtime_r(&seconds, &zulu);
-    if (pointer == (struct tm *)0) { perror("localtime_r"); }
+    pointer = gmtime_r(&seconds, &zulu);
+    if (pointer == (struct tm *)0) { perror("gmtime_r"); }
     printf("zulu=%d/%d/%dT%d:%d:%d+%d\n", zulu.tm_year + 1900, zulu.tm_mon, zulu.tm_mday, zulu.tm_hour, zulu.tm_min, zulu.tm_sec, zulu.tm_isdst);
 
-    seconds = problematic;
     pointer = localtime_r(&seconds, &juliet);
     if (pointer == (struct tm *)0) { perror("localtime_r"); }
     printf("juliet=%d/%d/%dT%d:%d:%d+%d\n", juliet.tm_year + 1900, juliet.tm_mon, juliet.tm_mday, juliet.tm_hour, juliet.tm_min, juliet.tm_sec, juliet.tm_isdst);
 
-    problematic = 0xffffffffffffffffLL;
-    printf("problematic=%lld=0x%llx\n", (long long)problematic, (long long)problematic);
+    seconds = (time_t)0xffffffffffffffffLL;
+    printf("seconds=%ld=0x%08lx\n", (long)seconds, (long)seconds);
 
-    seconds = problematic;
-    pointer = localtime_r(&seconds, &zulu);
-    if (pointer == (struct tm *)0) { perror("localtime_r"); }
+    pointer = gmtime_r(&seconds, &zulu);
+    if (pointer == (struct tm *)0) { perror("gmtime_r"); }
     printf("zulu=%d/%d/%dT%d:%d:%d+%d\n", zulu.tm_year + 1900, zulu.tm_mon, zulu.tm_mday, zulu.tm_hour, zulu.tm_min, zulu.tm_sec, zulu.tm_isdst);
 
-    seconds = problematic;
     pointer = localtime_r(&seconds, &juliet);
     if (pointer == (struct tm *)0) { perror("localtime_r"); }
     printf("juliet=%d/%d/%dT%d:%d:%d+%d\n", juliet.tm_year + 1900, juliet.tm_mon, juliet.tm_mday, juliet.tm_hour, juliet.tm_min, juliet.tm_sec, juliet.tm_isdst);
 
-    problematic = 0x000000000000000LL;
-    printf("problematic=%lld=0x%llx\n", (long long)problematic, (long long)problematic);
+    seconds = (time_t)0x000000000000000LL;
+    printf("seconds=%ld=0x%08lx\n", (long)seconds, (long)seconds);
 
-    seconds = problematic;
-    pointer = localtime_r(&seconds, &zulu);
-    if (pointer == (struct tm *)0) { perror("localtime_r"); }
+    pointer = gmtime_r(&seconds, &zulu);
+    if (pointer == (struct tm *)0) { perror("gmtime_r"); }
     printf("zulu=%d/%d/%dT%d:%d:%d+%d\n", zulu.tm_year + 1900, zulu.tm_mon, zulu.tm_mday, zulu.tm_hour, zulu.tm_min, zulu.tm_sec, zulu.tm_isdst);
 
-    seconds = problematic;
     pointer = localtime_r(&seconds, &juliet);
     if (pointer == (struct tm *)0) { perror("localtime_r"); }
     printf("juliet=%d/%d/%dT%d:%d:%d+%d\n", juliet.tm_year + 1900, juliet.tm_mon, juliet.tm_mday, juliet.tm_hour, juliet.tm_min, juliet.tm_sec, juliet.tm_isdst);
 
-    problematic = 0x000000007fffffffLL;
-    printf("problematic=%lld=0x%llx\n", (long long)problematic, (long long)problematic);
+    seconds = (time_t)0x000000007fffffffLL;
+    printf("seconds=%ld=0x%08lx\n", (long)seconds, (long)seconds);
 
-    seconds = problematic;
-    pointer = localtime_r(&seconds, &zulu);
-    if (pointer == (struct tm *)0) { perror("localtime_r"); }
+    pointer = gmtime_r(&seconds, &zulu);
+    if (pointer == (struct tm *)0) { perror("gmtime_r"); }
     printf("zulu=%d/%d/%dT%d:%d:%d+%d\n", zulu.tm_year + 1900, zulu.tm_mon, zulu.tm_mday, zulu.tm_hour, zulu.tm_min, zulu.tm_sec, zulu.tm_isdst);
 
-    seconds = problematic;
     pointer = localtime_r(&seconds, &juliet);
     if (pointer == (struct tm *)0) { perror("localtime_r"); }
     printf("juliet=%d/%d/%dT%d:%d:%d+%d\n", juliet.tm_year + 1900, juliet.tm_mon, juliet.tm_mday, juliet.tm_hour, juliet.tm_min, juliet.tm_sec, juliet.tm_isdst);
 
-    earlier.tm_year = 1901 - 1900;
-    earlier.tm_mon = 12 - 1;
-    earlier.tm_mday = 13;
-    earlier.tm_hour = 13;
-    earlier.tm_min = 45;
-    earlier.tm_sec = 52;
-    earlier.tm_isdst = 0;
-    printf("earlier=%d/%d/%dT%d:%d:%d+%d\n", earlier.tm_year + 1900, earlier.tm_mon, earlier.tm_mday, earlier.tm_hour, earlier.tm_min, earlier.tm_sec, earlier.tm_isdst);
+    data.tm_year = 1901 - 1900;
+    data.tm_mon = 12 - 1;
+    data.tm_mday = 13;
+    data.tm_hour = 13;
+    data.tm_min = 45;
+    data.tm_sec = 52;
+    data.tm_isdst = 0;
+    printf("data=%d/%d/%dT%d:%d:%d+%d\n", data.tm_year + 1900, data.tm_mon, data.tm_mday, data.tm_hour, data.tm_min, data.tm_sec, data.tm_isdst);
 
-    seconds = timegm(&earlier);
+    seconds = timegm(&data);
     if (seconds == (time_t)-1) { perror("timegm"); }
-    printf("seconds=%ld=0x%lx\n", seconds, seconds);
+    printf("seconds=%ld=0x%08lx\n", seconds, seconds);
 
-    pointer = localtime_r(&seconds, &later);
+    pointer = gmtime_r(&seconds, &zulu);
     if (pointer == (struct tm *)0) { perror("localtime_r"); }
-    printf("later=%d/%d/%dT%d:%d:%d+%d\n", later.tm_year + 1900, later.tm_mon, later.tm_mday, later.tm_hour, later.tm_min, later.tm_sec, later.tm_isdst);
+    printf("zulu=%d/%d/%dT%d:%d:%d+%d\n", zulu.tm_year + 1900, zulu.tm_mon, zulu.tm_mday, zulu.tm_hour, zulu.tm_min, zulu.tm_sec, zulu.tm_isdst);
+
+    seconds = timelocal(&data);
+    if (seconds == (time_t)-1) { perror("timelocal"); }
+    printf("seconds=%ld=0x%08lx\n", seconds, seconds);
+
+    pointer = localtime_r(&seconds, &juliet);
+    if (pointer == (struct tm *)0) { perror("localtime_r"); }
+    printf("juliet=%d/%d/%dT%d:%d:%d+%d\n", juliet.tm_year + 1900, juliet.tm_mon, juliet.tm_mday, juliet.tm_hour, juliet.tm_min, juliet.tm_sec, juliet.tm_isdst);
 }
