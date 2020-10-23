a82c5230e4be0a9c67f07d10861a0b37420748ca
Chip Overclock <coverclock@diag.com>
2020-10-23 17:42:12 +0000

    [1] Added some prompts to pintest so I don't have to look at the code. TESTED

--- Diminuto/fun/pintest.sh
+++ Diminuto/fun/pintest.sh
@@ -11,25 +11,28 @@
 # displayed on the LEDs, or press the button pn pin 22 simultaneously
 # with pin 27 to exit. You will probably have to run this as root.
 
+PROGRAM=$(basename $0)
+
 . $(readlink -e $(dirname ${0})/../fun)/hardware_test_fixture
 
 . $(readlink -e $(dirname ${0})/../bin)/setup
 
-trap "pintool -p ${HARDWARE_TEST_FIXTURE_PIN_LED_1} -e; pintool -p ${HARDWARE_TEST_FIXTURE_PIN_LED_2} -e; pintool -p ${HARDWARE_TEST_FIXTURE_PIN_LED_3} -e; pintool -p ${HARDWARE_TEST_FIXTURE_PIN_PWM_LED} -e; pintool -p ${HARDWARE_TEST_FIXTURE_PIN_BUT_LOW} -e; pintool -p ${HARDWARE_TEST_FIXTURE_PIN_BUT_HIGH} -e" 0 2 3 15
+trap "pintool -p ${HARDWARE_TEST_FIXTURE_PIN_LED_1} -e; pintool -p ${HARDWARE_TEST_FIXTURE_PIN_LED_2} -e; pintool -p ${HARDWARE_TEST_FIXTURE_PIN_LED_3} -e; pintool -p ${HARDWARE_TEST_FIXTURE_PIN_BUT_LOW} -e; pintool -p ${HARDWARE_TEST_FIXTURE_PIN_BUT_HIGH} -e" 0 2 3 15
+
+echo "${PROGRAM}: initializing."
 
 pintool -p ${HARDWARE_TEST_FIXTURE_PIN_LED_1} -e -x -u 1000000 -o
 pintool -p ${HARDWARE_TEST_FIXTURE_PIN_LED_2} -e -x -u 1000000 -o
 pintool -p ${HARDWARE_TEST_FIXTURE_PIN_LED_3} -e -x -u 1000000 -o
-pintool -p ${HARDWARE_TEST_FIXTURE_PIN_PWM_LED} -e -x -u 1000000 -o
 pintool -p ${HARDWARE_TEST_FIXTURE_PIN_BUT_LOW} -e -x -u 1000000 -i -L
 pintool -p ${HARDWARE_TEST_FIXTURE_PIN_BUT_HIGH} -e -x -u 1000000 -i -H
 
 PIN22=$(pintool -p ${HARDWARE_TEST_FIXTURE_PIN_BUT_LOW} -r)
-echo PIN22=${PIN22}
+echo "${PROGRAM}: button ${HARDWARE_TEST_FIXTURE_PIN_BUT_LOW} is ${PIN22}"
 PIN27=$(pintool -p ${HARDWARE_TEST_FIXTURE_PIN_BUT_HIGH} -r)
-echo PIN27=${PIN27}
+echo "${PROGRAM}: button ${HARDWARE_TEST_FIXTURE_PIN_BUT_HIGH} is ${PIN27}"
 
-pintool -p ${HARDWARE_TEST_FIXTURE_PIN_PWM_LED} -s
+echo "${PROGRAM}: cycling LEDs ${HARDWARE_TEST_FIXTURE_PIN_LED_1} ${HARDWARE_TEST_FIXTURE_PIN_LED_2} ${HARDWARE_TEST_FIXTURE_PIN_LED_3}."
 
 pintool -p ${HARDWARE_TEST_FIXTURE_PIN_LED_1} -s
 sleep 1
@@ -43,6 +46,9 @@ pintool -p ${HARDWARE_TEST_FIXTURE_PIN_LED_2} -c
 sleep 1
 pintool -p ${HARDWARE_TEST_FIXTURE_PIN_LED_3} -c
 sleep 1
+
+echo "${PROGRAM}: press button ${HARDWARE_TEST_FIXTURE_PIN_BUT_HIGH} to advance."
+echo "${PROGRAM}: also press button ${HARDWARE_TEST_FIXTURE_PIN_BUT_LOW} to exit."
 
 VALUE=0
 pintool -p ${HARDWARE_TEST_FIXTURE_PIN_BUT_HIGH} -b 10000 | while read PIN27; do
@@ -68,6 +74,6 @@ pintool -p ${HARDWARE_TEST_FIXTURE_PIN_BUT_HIGH} -b 10000 | while read PIN27; do
 	fi
 done
 
-pintool -p ${HARDWARE_TEST_FIXTURE_PIN_PWM_LED} -c
+echo "${PROGRAM}: exiting."
 
 exit 0
