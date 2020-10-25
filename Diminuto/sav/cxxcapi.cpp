fec2f490dbb3c50d279344b27f1eed20285eb8f4
Chip Overclock <coverclock@diag.com>
2020-10-25 17:13:07 -0600

    [1] Added some actual executable code to cxxcapi. TESTED

--- Diminuto/sav/cxxcapi.cpp
+++ Diminuto/sav/cxxcapi.cpp
@@ -1,14 +1,19 @@
+#if 0
+g++ -o cxxcapi -I inc sav/cxxcapi.cpp out/host/arc/libdiminuto.a -lpthread -lrt -ldl
+#endif
+
 #include "com/diag/diminuto/diminuto_cxxcapi.h"
 
 #include "com/diag/diminuto/diminuto_begin.h"
 #include "com/diag/diminuto/diminuto_mux.h"
 #include "com/diag/diminuto/diminuto_timer.h"
 #include "com/diag/diminuto/diminuto_list.h"
+#include "com/diag/diminuto/diminuto_log.h"
 #include "com/diag/diminuto/diminuto_end.h"
 
 #include <stdio.h>
 
 int main(void)
 {
-    printf("Hello, world!\n");
+    DIMINUTO_LOG_NOTICE("Hello, world!\n");
 }
