cae8ec4cd703f61a2792cf7875587377f5d9c872
Chip Overclock <coverclock@diag.com>
2020-10-31 15:04:26 -0600

    [1] Added actual unit test code to guards unit test.

--- Diminuto/tst/unittest-guards.sh
+++ Diminuto/tst/unittest-guards.sh
@@ -10,8 +10,8 @@ ARCDIR=$(readlink -e $(dirname ${0})/../arc)
 INCDIR=$(readlink -e $(dirname ${0})/../../../inc)
 GENDIR=$(readlink -e $(dirname ${0})/../inc)
 SRCFIL=$(mktemp ${TMPDIR:-"/tmp"}/$(basename ${0} .sh)-XXXXXXXXXX.c)
-EXEFIL=$(mktemp ${TMPDIR:-"/tmp"}/$(basename ${0} .sh)-XXXXXXXXXX.exe)
-trap "rm -f ${SRCFIL} ${EXEFIL}" 0 1 2 3 15
+OUTFIL=$(mktemp ${TMPDIR:-"/tmp"}/$(basename ${0} .sh)-XXXXXXXXXX.out)
+trap "rm -f ${SRCFIL} ${OUTFIL}" 0 1 2 3 15
 find ${INCDIR} ${GENDIR} -type f -name '*.h' -print | awk '
 	{ print "#include \""$1"\""; }
 	{ print "#include \""$1"\""; }
@@ -20,5 +20,5 @@ cat << EOF >> ${SRCFIL}
 int main(void) { TEST(); STATUS(); EXIT(); }
 EOF
 cat -n ${SRCFIL}
-gcc -I ${INCDIR} -I ${GENDIR} -D__USE_GNU -D_GNU_SOURCE -o ${EXEFIL} ${SRCFIL} ${ARCDIR}/libdiminuto.a -lpthread -lrt -ldl  || exit 1
-${EXEFIL}
+gcc -I ${INCDIR} -I ${GENDIR} -D__USE_GNU -D_GNU_SOURCE -o ${OUTFIL} ${SRCFIL} ${ARCDIR}/libdiminuto.a -lpthread -lrt -ldl  || exit 1
+${OUTFIL}
