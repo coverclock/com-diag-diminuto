0253898497a0cf8759f36ba27cc562abaec63446
Chip Overclock <coverclock@diag.com>
2020-11-02 11:59:54 -0700

    [1] Fixed misssing paranthesis in swap. TESTED [2] Expanded swap unit test very slightly. PASSED [3] Bumped release.

--- Diminuto/inc/com/diag/diminuto/diminuto_swap.h
+++ Diminuto/inc/com/diag/diminuto/diminuto_swap.h
@@ -19,13 +19,15 @@
  * @def DIMINUTO_SWAP
  * Swap the contents of variables @a _A_ and @a _B_ that are both of the same
  * integer type without using any temporary storage. This is a clever
- * hack applicable to only the most resource constrained environments.
+ * hack applicable to only the most resource constrained environments. The
+ * arguments are each evaluated three times, so side effects may cause
+ * wackiness to ensue.
  */
 #define DIMINUTO_SWAP(_A_, _B_) \
 	do { \
-		_A_ ^= _B_; \
-		_B_ ^= _A_; \
-		_A_ ^= _B_; \
+		(_A_) ^= (_B_); \
+		(_B_) ^= (_A_); \
+		(_A_) ^= (_B_); \
 	} while (0)
 
 #endif /* _H_COM_DIAG_DIMINUTO_SWAP_ */
