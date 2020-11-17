0cdf90bbdde2f708ad9f8812e0b74df99fef024e
Chip Overclock <coverclock@diag.com>
2020-11-17 09:34:27 -0700

    [1] Added unit test for operator macros. PASSED [2] Added operator macros unit test suite to sanity. TESTED

--- Diminuto/inc/com/diag/diminuto/diminuto_macros.h
+++ Diminuto/inc/com/diag/diminuto/diminuto_macros.h
@@ -16,28 +16,40 @@
  * To avoid collisions with the application in the C preprocessor name space,
  * macros that are intended to be used by the application have relatively simple
  * names, while those that are used internally have names beginning with
- * COM_DIAG_DIMINUTO_.
+ * COM_DIAG_DIMINUTO_. (Some of these macros are exposed with simple names
+ * mostly for unit testing.)
+ *
+ * I really admire Dr. Heathcoat; not only am I pretty sure I would never have
+ * figured this out, I'm pretty certain I would never thought to do it in the
+ * first place.
+ *
+ * See the unit test for some examples of how to use these macros.
  *
  * REFERENCES
  *
  * J. Heathcote, "C Pre-Processor Magic", 2020,
  * <http://jhnet.co.uk/articles/cpp_magic>
+ *
+ * Wikipedia, "X-Macro", <https://en.wikipedia.org/wiki/X_Macro>
+ *
+ * Wikibooks, "C Programming/Preprocessor directives and macros",
+ * <https://en.wikibooks.org/wiki/C_Programming/Preprocessor_directives_and_macros#X-Macros>
  */
 
 /*******************************************************************************
- *
+ * PUBLIC
  ******************************************************************************/
 
 #define EMPTY()
 
-#define CAT(_A_, _B_) _A_ ## _B_
+#define CONCATENATE(_A_, _B_) _A_ ## _B_
 
 #define FIRST(_FIRST_, ...) _FIRST_
 
 #define SECOND(_FIRST_, _SECOND_, ...) _SECOND_
 
 /*******************************************************************************
- *
+ * PRIVATE
  ******************************************************************************/
 
 #define COM_DIAG_DIMINUTO_IS_COM_DIAG_DIMINUTO_PROBE(...) SECOND(__VA_ARGS__, 0)
@@ -47,15 +59,15 @@
 #define COM_DIAG_DIMINUTO_NOT_0 COM_DIAG_DIMINUTO_PROBE()
 
 /*******************************************************************************
- *
+ * PUBLIC
  ******************************************************************************/
 
-#define NOT(_X_) COM_DIAG_DIMINUTO_IS_COM_DIAG_DIMINUTO_PROBE(CAT(COM_DIAG_DIMINUTO_NOT_, _X_))
+#define NOT(_X_) COM_DIAG_DIMINUTO_IS_COM_DIAG_DIMINUTO_PROBE(CONCATENATE(COM_DIAG_DIMINUTO_NOT_, _X_))
 
 #define BOOL(_X_) NOT(NOT(_X_))
 
 /*******************************************************************************
- *
+ * PRIVATE
  ******************************************************************************/
 
 #define COM_DIAG_DIMINUTO_IF_1_ELSE(...)
@@ -64,50 +76,51 @@
 #define COM_DIAG_DIMINUTO_IF_1(...) __VA_ARGS__ COM_DIAG_DIMINUTO_IF_1_ELSE
 #define COM_DIAG_DIMINUTO_IF_0(...)             COM_DIAG_DIMINUTO_IF_0_ELSE
 
-#define COM_DIAG_DIMINUTO_IF_ELSE(_CONDITION_) CAT(COM_DIAG_DIMINUTO_IF_, _CONDITION_)
+#define COM_DIAG_DIMINUTO_IF_ELSE(_CONDITION_) CONCATENATE(COM_DIAG_DIMINUTO_IF_, _CONDITION_)
 
 /*******************************************************************************
- *
+ * PUBLIC
  ******************************************************************************/
 
 #define IF_ELSE(_CONDITION_) COM_DIAG_DIMINUTO_IF_ELSE(BOOL(_CONDITION_))
 
 /*******************************************************************************
- *
+ * PRIVATE
  ******************************************************************************/
 
-#define FORALL(...) FORALL1024(__VA_ARGS__)
-#define FORALL1024(...) FORALL512(FORALL512(__VA_ARGS__))
-#define FORALL512(...) FORALL256(FORALL256(__VA_ARGS__))
-#define FORALL256(...) FORALL128(FORALL128(__VA_ARGS__))
-#define FORALL128(...) FORALL64(FORALL64(__VA_ARGS__))
-#define FORALL64(...) FORALL32(FORALL32(__VA_ARGS__))
-#define FORALL32(...) FORALL16(FORALL16(__VA_ARGS__))
-#define FORALL16(...) FORALL8(FORALL8(__VA_ARGS__))
-#define FORALL8(...) FORALL4(FORALL4(__VA_ARGS__))
-#define FORALL4(...) FORALL2(FORALL2(__VA_ARGS__))
-#define FORALL2(...) FORALL1(FORALL1(__VA_ARGS__))
-#define FORALL1(...) __VA_ARGS__
+#define COM_DIAG_DIMINUTO_FORALL1024(...) COM_DIAG_DIMINUTO_FORALL512(COM_DIAG_DIMINUTO_FORALL512(__VA_ARGS__))
+#define COM_DIAG_DIMINUTO_FORALL512(...) COM_DIAG_DIMINUTO_FORALL256(COM_DIAG_DIMINUTO_FORALL256(__VA_ARGS__))
+#define COM_DIAG_DIMINUTO_FORALL256(...) COM_DIAG_DIMINUTO_FORALL128(COM_DIAG_DIMINUTO_FORALL128(__VA_ARGS__))
+#define COM_DIAG_DIMINUTO_FORALL128(...) COM_DIAG_DIMINUTO_FORALL64(COM_DIAG_DIMINUTO_FORALL64(__VA_ARGS__))
+#define COM_DIAG_DIMINUTO_FORALL64(...) COM_DIAG_DIMINUTO_FORALL32(COM_DIAG_DIMINUTO_FORALL32(__VA_ARGS__))
+#define COM_DIAG_DIMINUTO_FORALL32(...) COM_DIAG_DIMINUTO_FORALL16(COM_DIAG_DIMINUTO_FORALL16(__VA_ARGS__))
+#define COM_DIAG_DIMINUTO_FORALL16(...) COM_DIAG_DIMINUTO_FORALL8(COM_DIAG_DIMINUTO_FORALL8(__VA_ARGS__))
+#define COM_DIAG_DIMINUTO_FORALL8(...) COM_DIAG_DIMINUTO_FORALL4(COM_DIAG_DIMINUTO_FORALL4(__VA_ARGS__))
+#define COM_DIAG_DIMINUTO_FORALL4(...) COM_DIAG_DIMINUTO_FORALL2(COM_DIAG_DIMINUTO_FORALL2(__VA_ARGS__))
+#define COM_DIAG_DIMINUTO_FORALL2(...) COM_DIAG_DIMINUTO_FORALL1(COM_DIAG_DIMINUTO_FORALL1(__VA_ARGS__))
+#define COM_DIAG_DIMINUTO_FORALL1(...) __VA_ARGS__
 
-#define DEFER1(_M_) _M_ EMPTY()
-#define DEFER2(_M_) _M_ EMPTY EMPTY()()
-#define DEFER3(_M_) _M_ EMPTY EMPTY EMPTY()()()
-#define DEFER4(_M_) _M_ EMPTY EMPTY EMPTY EMPTY()()()()
+#define COM_DIAG_DIMINUTO_DEFER1(_M_) _M_ EMPTY()
+#define COM_DIAG_DIMINUTO_DEFER2(_M_) _M_ EMPTY EMPTY()()
+#define COM_DIAG_DIMINUTO_DEFER3(_M_) _M_ EMPTY EMPTY EMPTY()()()
+#define COM_DIAG_DIMINUTO_DEFER4(_M_) _M_ EMPTY EMPTY EMPTY EMPTY()()()()
 
 #define COM_DIAG_DIMINUTO_END_OF_ARGUMENTS_() 0
-
-#define HAS_ARGS(...) BOOL(FIRST(COM_DIAG_DIMINUTO_END_OF_ARGUMENTS_ __VA_ARGS__)())
 
 #define COM_DIAG_DIMINUTO_APPLY() APPLY
 
 /*******************************************************************************
- *
+ * PUBLIC
  ******************************************************************************/
 
-#define APPLY(_M_, _FIRST_, ...) _M_(_FIRST_) IF_ELSE(HAS_ARGS(__VA_ARGS__))(DEFER2(COM_DIAG_DIMINUTO_APPLY)()(_M_, __VA_ARGS__))(/**/)
+#define HAS_ARGUMENTS(...) BOOL(FIRST(COM_DIAG_DIMINUTO_END_OF_ARGUMENTS_ __VA_ARGS__)())
+
+#define FORALL(...) COM_DIAG_DIMINUTO_FORALL1024(__VA_ARGS__)
+
+#define APPLY(_M_, _FIRST_, ...) _M_(_FIRST_) IF_ELSE(HAS_ARGUMENTS(__VA_ARGS__))(COM_DIAG_DIMINUTO_DEFER2(COM_DIAG_DIMINUTO_APPLY)()(_M_, __VA_ARGS__))(/**/)
 
 /*******************************************************************************
- *
+ * END
  ******************************************************************************/
 
 #endif
---
+++ Diminuto/tst/unittest-macros.c
@@ -0,0 +1,120 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+/**
+ * @file
+ * @copyright Copyright 2020 Digital Aggregates Corporation, Colorado, USA.
+ * @note Licensed under the terms in LICENSE.txt.
+ * @brief
+ * @author Chip Overclock <mailto:coverclock@diag.com>
+ * @see Diminuto <https://github.com/coverclock/com-diag-diminuto>
+ * @details
+ */
+
+#include "com/diag/diminuto/diminuto_unittest.h"
+#include "com/diag/diminuto/diminuto_macros.h"
+
+static int result(int arg)
+{
+    COMMENT("result=%d\n", arg);
+    return arg;
+}
+
+static int total = 0;
+
+static void square(int arg)
+{
+    int squared;
+
+    squared = arg * arg;
+    COMMENT("%d^2=%d\n", arg, squared);
+    total += squared;
+}
+
+static void display(const char * arg)
+{
+    COMMENT("string=\"%s\"\n", arg);
+}
+
+int main(void)
+{
+    SETLOGMASK();
+
+    {
+        TEST();
+
+        ASSERT(CONCATENATE(5, 7)  == 57);
+        ASSERT(FIRST(2, 3, 5, 7) == 2);
+        ASSERT(SECOND(2, 3, 5, 7) == 3);
+
+        STATUS();
+    }
+
+    {
+        TEST();
+
+        ASSERT(BOOL(0) == 0);
+        ASSERT(BOOL(1) == 1);
+        ASSERT(BOOL(123) == 1);
+        ASSERT(BOOL(anything) == 1);
+
+        STATUS();
+    }
+
+    {
+        TEST();
+
+        ASSERT(NOT(0) == 1);
+        ASSERT(NOT(1) == 0);
+        ASSERT(NOT(123) == 0);
+        ASSERT(NOT(anything) == 0);
+
+        STATUS();
+    }
+
+    {
+        TEST();
+
+        ASSERT((IF_ELSE(1)(123)(456)) == 123);
+        ASSERT((IF_ELSE(0)(123)(456)) == 456);
+
+        ASSERT((IF_ELSE(1)(result(123))(result(456))) == 123);
+        ASSERT((IF_ELSE(0)(result(123))(result(456))) == 456);
+
+        STATUS();
+    }
+
+    {
+        TEST();
+
+        ASSERT(HAS_ARGUMENTS(a, b, c) == 1);
+        ASSERT(HAS_ARGUMENTS() == 0);
+
+        STATUS();
+    }
+
+    {
+        TEST();
+
+#define SQUARE(_X_) square(_X_);
+
+        total = 0;
+        FORALL(APPLY(SQUARE, 2, 3, 5, 7, 9))
+        ASSERT(total == 168);
+
+        STATUS();
+    }
+
+    {
+        TEST();
+        const char B[]="BB";
+        const char * D = "DDDD";
+
+#define DISPLAY(_STRING_) display(_STRING_);
+
+        FORALL(APPLY(DISPLAY, "A", B, "CCC", D));
+
+        STATUS();
+    }
+
+    EXIT();
+
+}
