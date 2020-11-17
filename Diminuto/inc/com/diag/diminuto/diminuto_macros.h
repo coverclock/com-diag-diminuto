269fb54db10eec01066c6a52b8ad87c68863f405
Chip Overclock <coverclock@diag.com>
2020-11-17 09:02:34 -0700

    [1] Checkpoint

--- Diminuto/inc/com/diag/diminuto/diminuto_macros.h
+++ Diminuto/inc/com/diag/diminuto/diminuto_macros.h
@@ -6,10 +6,17 @@
  * @file
  * @copyright Copyright 2020 Digital Aggregates Corporation, Colorado, USA.
  * @note Licensed under the terms in LICENSE.txt.
- * @brief
+ * @brief Implement IF ELSE, EVAL, and APPLY preprocessor macros.
  * @author Chip Overclock <mailto:coverclock@diag.com>
  * @see Diminuto <https://github.com/coverclock/com-diag-diminuto>
  * @details
+ * Implement IF ELSE, EVAL, and APPLY preprocessor macros as described by
+ * Jonathan Heathcoat of BBC R&D.
+ *
+ * To avoid collisions with the application in the C preprocessor name space,
+ * macros that are intended to be used by the application have relatively simple
+ * names, while those that are used internally have names beginning with
+ * COM_DIAG_DIMINUTO_.
  *
  * REFERENCES
  *
@@ -21,67 +28,83 @@
  *
  ******************************************************************************/
 
+#define EMPTY()
+
 #define CAT(_A_, _B_) _A_ ## _B_
 
 #define FIRST(_FIRST_, ...) _FIRST_
 
 #define SECOND(_FIRST_, _SECOND_, ...) _SECOND_
 
-#define IS_PROBE(...) SECOND(__VA_ARGS__, 0)
+/*******************************************************************************
+ *
+ ******************************************************************************/
 
-#define PROBE() ~, 1
+#define COM_DIAG_DIMINUTO_IS_COM_DIAG_DIMINUTO_PROBE(...) SECOND(__VA_ARGS__, 0)
 
-#define _NOT_0 PROBE()
+#define COM_DIAG_DIMINUTO_PROBE() ~, 1
 
-#define NOT(_X_) IS_PROBE(CAT(_NOT_, _X_))
+#define COM_DIAG_DIMINUTO_NOT_0 COM_DIAG_DIMINUTO_PROBE()
+
+/*******************************************************************************
+ *
+ ******************************************************************************/
+
+#define NOT(_X_) COM_DIAG_DIMINUTO_IS_COM_DIAG_DIMINUTO_PROBE(CAT(COM_DIAG_DIMINUTO_NOT_, _X_))
 
 #define BOOL(_X_) NOT(NOT(_X_))
 
-#define EMPTY()
+/*******************************************************************************
+ *
+ ******************************************************************************/
+
+#define COM_DIAG_DIMINUTO_IF_1_ELSE(...)
+#define COM_DIAG_DIMINUTO_IF_0_ELSE(...) __VA_ARGS__
+
+#define COM_DIAG_DIMINUTO_IF_1(...) __VA_ARGS__ COM_DIAG_DIMINUTO_IF_1_ELSE
+#define COM_DIAG_DIMINUTO_IF_0(...)             COM_DIAG_DIMINUTO_IF_0_ELSE
+
+#define COM_DIAG_DIMINUTO_IF_ELSE(_CONDITION_) CAT(COM_DIAG_DIMINUTO_IF_, _CONDITION_)
 
 /*******************************************************************************
  *
  ******************************************************************************/
 
-#define _IF_1_ELSE(...)
-#define _IF_0_ELSE(...) __VA_ARGS__
-
-#define _IF_1(...) __VA_ARGS__ _IF_1_ELSE
-#define _IF_0(...)             _IF_0_ELSE
-
-#define _IF_ELSE(_CONDITION_) CAT(_IF_, _CONDITION_)
-
-#define IF_ELSE(_CONDITION_) _IF_ELSE(BOOL(_CONDITION_))
+#define IF_ELSE(_CONDITION_) COM_DIAG_DIMINUTO_IF_ELSE(BOOL(_CONDITION_))
 
 /*******************************************************************************
  *
  ******************************************************************************/
 
-#define EVAL(...) EVAL1024(__VA_ARGS__)
-#define EVAL1024(...) EVAL512(EVAL512(__VA_ARGS__))
-#define EVAL512(...) EVAL256(EVAL256(__VA_ARGS__))
-#define EVAL256(...) EVAL128(EVAL128(__VA_ARGS__))
-#define EVAL128(...) EVAL64(EVAL64(__VA_ARGS__))
-#define EVAL64(...) EVAL32(EVAL32(__VA_ARGS__))
-#define EVAL32(...) EVAL16(EVAL16(__VA_ARGS__))
-#define EVAL16(...) EVAL8(EVAL8(__VA_ARGS__))
-#define EVAL8(...) EVAL4(EVAL4(__VA_ARGS__))
-#define EVAL4(...) EVAL2(EVAL2(__VA_ARGS__))
-#define EVAL2(...) EVAL1(EVAL1(__VA_ARGS__))
-#define EVAL1(...) __VA_ARGS__
+#define FORALL(...) FORALL1024(__VA_ARGS__)
+#define FORALL1024(...) FORALL512(FORALL512(__VA_ARGS__))
+#define FORALL512(...) FORALL256(FORALL256(__VA_ARGS__))
+#define FORALL256(...) FORALL128(FORALL128(__VA_ARGS__))
+#define FORALL128(...) FORALL64(FORALL64(__VA_ARGS__))
+#define FORALL64(...) FORALL32(FORALL32(__VA_ARGS__))
+#define FORALL32(...) FORALL16(FORALL16(__VA_ARGS__))
+#define FORALL16(...) FORALL8(FORALL8(__VA_ARGS__))
+#define FORALL8(...) FORALL4(FORALL4(__VA_ARGS__))
+#define FORALL4(...) FORALL2(FORALL2(__VA_ARGS__))
+#define FORALL2(...) FORALL1(FORALL1(__VA_ARGS__))
+#define FORALL1(...) __VA_ARGS__
 
 #define DEFER1(_M_) _M_ EMPTY()
 #define DEFER2(_M_) _M_ EMPTY EMPTY()()
 #define DEFER3(_M_) _M_ EMPTY EMPTY EMPTY()()()
 #define DEFER4(_M_) _M_ EMPTY EMPTY EMPTY EMPTY()()()()
 
-#define _END_OF_ARGUMENTS_() 0
+#define COM_DIAG_DIMINUTO_END_OF_ARGUMENTS_() 0
 
-#define HAS_ARGS(...) BOOL(FIRST(_END_OF_ARGUMENTS_ __VA_ARGS__)())
+#define HAS_ARGS(...) BOOL(FIRST(COM_DIAG_DIMINUTO_END_OF_ARGUMENTS_ __VA_ARGS__)())
 
-#define MAP(_M_, _FIRST_, ...) _M_(_FIRST_) IF_ELSE(HAS_ARGS(__VA_ARGS__))(DEFER2(_MAP)()(_M_, __VA_ARGS__))(/**/)
+#define COM_DIAG_DIMINUTO_APPLY() APPLY
 
-#define _MAP() MAP
+/*******************************************************************************
+ *
+ ******************************************************************************/
+
+#define APPLY(_M_, _FIRST_, ...) _M_(_FIRST_) IF_ELSE(HAS_ARGS(__VA_ARGS__))(DEFER2(COM_DIAG_DIMINUTO_APPLY)()(_M_, __VA_ARGS__))(/**/)
 
 /*******************************************************************************
  *
