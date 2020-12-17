942f017d4064f65af8cb9fce057c5822d0b87476
Chip Overclock <coverclock@diag.com>
2020-12-17 14:33:26 -0700

    [1] Checkpoint. BUILDS FAILS

--- Diminuto/inc/com/diag/diminuto/diminuto_bits.h
+++ Diminuto/inc/com/diag/diminuto/diminuto_bits.h
@@ -11,24 +11,41 @@
  * @see Diminuto <https://github.com/coverclock/com-diag-diminuto>
  * @details
  * Provides helper macros (masquerading as functions) to compute bit
- * indices, offsets, and masks.
+ * indices, offsets, and masks. Works best with unsigned types,
+ * otherwise C may promote the value to a wider type and extend
+ * its sign. See the unit test for specific examples.
  */
 
 #include "com/diag/diminuto/diminuto_widthof.h"
+#include <sys/types.h>
+#include <stdint.h>
+
+typedef size_t diminuto_bits_size_t;
+
+typedef uint64_t diminuto_bits_mask_t;
 
 #define diminuto_bits_index(_TYPE_, _BIT_) \
-    ((_BIT_) / diminuto_widthof(_TYPE_))
+    (((diminuto_bits_size_t)(_BIT_)) / diminuto_widthof(_TYPE_))
 
 #define diminuto_bits_offset(_TYPE_, _BIT_) \
-    ((_BIT_) % diminuto_widthof(_TYPE_))
+    (((diminuto_bits_size_t)(_BIT_)) % diminuto_widthof(_TYPE_))
 
-#define diminuto_bits_mask(_TYPE_, _BIT_) \
-    (((_TYPE_)1) << diminuto_bits_offset(_TYPE_, _BIT_))
+#define diminuto_bits_mask(_TYPE_) \
+    ((_TYPE_)(~((diminuto_bits_mask_t)0)))
+
+#define diminuto_bits_or(_TYPE_, _BIT_) \
+    ((_TYPE_)(((_TYPE_)1) << diminuto_bits_offset(_TYPE_, _BIT_)))
+
+#define diminuto_bits_and(_TYPE_, _BIT_) \
+    ((_TYPE_)(~(((_TYPE_)1) << diminuto_bits_offset(_TYPE_, _BIT_))))
 
 #define diminuto_bits_get(_ARRAY_, _TYPE_, _BIT_) \
-    ((((_ARRAY_)[diminuto_bits_index(_TYPE_, _BIT)]) & diminuto_bits_mask(_TYPE_, _BIT_)) >> diminuto_bits_offset(_TYPE_, _BIT_))
+    ((_TYPE_)(((((_ARRAY_)[diminuto_bits_index(_TYPE_, _BIT)]) & diminuto_bits_one(_TYPE_, _BIT_)) >> diminuto_bits_offset(_TYPE_, _BIT_))))
 
-#define diminuto_bits_set(_ARRAY_, _TYPE_, _BIT_, _VALUE_) \
-    (((_ARRAY_)[diminuto_bits_index(_TYPE_, _BIT)]) |= diminuto_bits_mask(_TYPE_, _BIT_))
+#define diminuto_bits_set(_ARRAY_, _TYPE_, _BIT_) \
+    (((_ARRAY_)[diminuto_bits_index(_TYPE_, _BIT)]) |= diminuto_bits_one(_TYPE_, _BIT_))
+
+#define diminuto_bits_clear(_ARRAY_, _TYPE_, _BIT_) \
+    (((_ARRAY_)[diminuto_bits_index(_TYPE_, _BIT)]) &= diminuto_bits_zero(_TYPE_, _BIT_))
 
 #endif
--- Diminuto/tst/unittest-bits.c
+++ Diminuto/tst/unittest-bits.c
@@ -8,37 +8,70 @@
  * @see Diminuto <https://github.com/coverclock/com-diag-diminuto>
  * @details
  * This is the unit test for the Bits feature.
+ *
+ * C99 (6.3.1.1): "If an int can represent all values of the original
+ * type, the value is converted to an int; otherwise, it is converted
+ * to an unsigned int. These are called the integer promotions. All
+ * other types are unchanged by the integer promotions." What I don't
+ * understand yet is why C promotes the RHS with sign extension but
+ * not the LHS.
  */
 
 #include "com/diag/diminuto/diminuto_unittest.h"
 #include "com/diag/diminuto/diminuto_bits.h"
+#include "com/diag/diminuto/diminuto_widthof.h"
+#include "com/diag/diminuto/diminuto_countof.h"
+#include "com/diag/diminuto/diminuto_minmaxof.h"
+#include "com/diag/diminuto/diminuto_dump.h"
 #include <stdint.h>
+#include <endian.h>
 
-#if 0
+static inline uint8_t htobe8(uint8_t host_8bits) { return host_8bits; }
 
-#define diminuto_bits_index(_TYPE_, _BIT_) \
-    ((_BIT_) / diminuto_widthof(_TYPE_))
-
-#define diminuto_bits_offset(_TYPE_, _BIT_) \
-    ((_BIT_) % diminuto_widthof(_TYPE_))
-
-#define diminuto_bits_mask(_TYPE_, _BIT_) \
-    ((_TYPE_)(((_TYPE_)1) << diminuto_bits_offset(_TYPE_, _BIT_)))
-
-#define diminuto_bits_get(_ARRAY_, _TYPE_, _BIT_) \
-    ((((_ARRAY_)[diminuto_bits_index(_TYPE_, _BIT)]) & diminuto_bits_mask(_TYPE_, _BIT_)) >> diminuto_bits_offset(_TYPE_, _BIT_))
-
-#define diminuto_bits_set(_ARRAY_, _TYPE_, _BIT_, _VALUE_) \
-    (((_ARRAY_)[diminuto_bits_index(_TYPE_, _BIT)]) |= diminuto_bits_mask(_TYPE_, _BIT_))
-
-#endif
+#define REMARK(_TYPE_, _HTOBEF_) \
+        { \
+                _TYPE_ mm = diminuto_bits_mask(_TYPE_); \
+                _TYPE_ ol = diminuto_bits_or(_TYPE_, 0); \
+                _TYPE_ oh = diminuto_bits_or(_TYPE_, diminuto_widthof(_TYPE_) - 1); \
+                _TYPE_ al = diminuto_bits_and(_TYPE_, 0); \
+                _TYPE_ ah = diminuto_bits_and(_TYPE_, diminuto_widthof(_TYPE_) - 1); \
+                fprintf(stderr, "%sint%zu_t: offset=(%zu..%zu)\n", \
+                    diminuto_issigned(_TYPE_) ? "" : "u", diminuto_widthof(_TYPE_), \
+                    diminuto_bits_offset(_TYPE_, 0), diminuto_bits_offset(_TYPE_, diminuto_widthof(_TYPE_) - 1)); \
+                mm = _HTOBEF_(mm); \
+                ol = _HTOBEF_(ol); \
+                oh = _HTOBEF_(oh); \
+                al = _HTOBEF_(al); \
+                ah = _HTOBEF_(ah); \
+                diminuto_dump(stderr, &mm, sizeof(_TYPE_)); \
+                diminuto_dump(stderr, &ol, sizeof(_TYPE_)); \
+                diminuto_dump(stderr, &oh, sizeof(_TYPE_)); \
+                diminuto_dump(stderr, &al, sizeof(_TYPE_)); \
+                diminuto_dump(stderr, &ah, sizeof(_TYPE_)); \
+        }
 
 int main(void)
 {
     SETLOGMASK();
 
     {
-        int ii = 0;
+        TEST();
+
+        REMARK(uint8_t,  htobe8);
+        REMARK(uint16_t, htobe16);
+        REMARK(uint32_t, htobe32);
+        REMARK(uint64_t, htobe64);
+
+        REMARK(int8_t,  htobe8);
+        REMARK(int16_t, htobe16);
+        REMARK(int32_t, htobe32);
+        REMARK(int64_t, htobe64);
+
+        STATUS();
+    }
+
+    {
+        int ii;
 
         TEST();
 
@@ -86,7 +119,7 @@ int main(void)
     }
 
     {
-        int ii = 0;
+        int ii;
 
         TEST();
 
@@ -142,68 +175,153 @@ int main(void)
     }
 
     {
-        int ii = 0;
+        int ii;
+
+        TEST();
+
+        ASSERT(diminuto_bits_mask(uint8_t) == (uint8_t)0xff);
+        ASSERT(diminuto_bits_mask(uint16_t) == (uint16_t)0xffff);
+        ASSERT(diminuto_bits_mask(uint32_t) == (uint32_t)0xffffffff);
+        ASSERT(diminuto_bits_mask(uint64_t) == (uint64_t)0xffffffffffffffffLL);
+
+        ASSERT(diminuto_bits_mask(int8_t) == (int8_t)0xff);
+        ASSERT(diminuto_bits_mask(int16_t) == (int16_t)0xffff);
+        ASSERT(diminuto_bits_mask(int32_t) == (int32_t)0xffffffff);
+        ASSERT(diminuto_bits_mask(int64_t) == (int64_t)0xffffffffffffffffLL);
+
+        STATUS();
+    }
+
+    {
+        int ii;
 
         TEST();
 
         for (ii = 0; ii < diminuto_widthof(uint8_t); ++ii) {
             uint8_t mm = (uint8_t)1 << ii;
-            ASSERT(diminuto_bits_mask(uint8_t, ii) == mm);
-            ASSERT(diminuto_bits_mask(uint8_t, ii * diminuto_widthof(uint8_t)) == 1);
+            ASSERT(diminuto_bits_or(uint8_t, ii) == mm);
+            ASSERT(diminuto_bits_or(uint8_t, ii * diminuto_widthof(uint8_t)) == 1);
         }
 
         for (ii = 0; ii < diminuto_widthof(uint16_t); ++ii) {
             uint16_t mm = (uint16_t)1 << ii;
-            ASSERT(diminuto_bits_mask(uint16_t, ii) == mm);
-            ASSERT(diminuto_bits_mask(uint16_t, ii * diminuto_widthof(uint16_t)) == 1);
+            ASSERT(diminuto_bits_or(uint16_t, ii) == mm);
+            ASSERT(diminuto_bits_or(uint16_t, ii * diminuto_widthof(uint16_t)) == 1);
         }
 
         for (ii = 0; ii < diminuto_widthof(uint32_t); ++ii) {
             uint32_t mm = (uint32_t)1 << ii;
-            ASSERT(diminuto_bits_mask(uint32_t, ii) == mm);
-            ASSERT(diminuto_bits_mask(uint32_t, ii * diminuto_widthof(uint32_t)) == 1);
+            ASSERT(diminuto_bits_or(uint32_t, ii) == mm);
+            ASSERT(diminuto_bits_or(uint32_t, ii * diminuto_widthof(uint32_t)) == 1);
         }
 
         for (ii = 0; ii < diminuto_widthof(uint64_t); ++ii) {
             uint64_t mm = (uint64_t)1 << ii;
-            ASSERT(diminuto_bits_mask(uint64_t, ii) == mm);
-            ASSERT(diminuto_bits_mask(uint64_t, ii * diminuto_widthof(uint64_t)) == 1);
+            ASSERT(diminuto_bits_or(uint64_t, ii) == mm);
+            ASSERT(diminuto_bits_or(uint64_t, ii * diminuto_widthof(uint64_t)) == 1);
         }
 
-        /*
-         * This is probably standard behavior but it surprised me: C promots
-         * the RHS test value to an int and extends the sign (appropriately),
-         * but does not do the same for the value generated by the software
-         * under test. So I'm doing something right in the SWUT, but am
-         * unable to reproduce it in the unit test program itself.
-         */
-
         for (ii = 0; ii < diminuto_widthof(int8_t); ++ii) {
-            int8_t mm = (int8_t)1 << ii;
-            ASSERT(diminuto_bits_mask(int8_t, ii) == (mm & 0xff));
-            ASSERT(diminuto_bits_mask(int8_t, ii * diminuto_widthof(int8_t)) == 1);
+            int8_t mm = ((int8_t)(int8_t)1 << ii);
+            ASSERT(diminuto_bits_or(int8_t, ii) == (mm & 0xff));
+            ASSERT(diminuto_bits_or(int8_t, ii * diminuto_widthof(int8_t)) == 1);
         }
 
         for (ii = 0; ii < diminuto_widthof(int16_t); ++ii) {
             int16_t mm = (int16_t)1 << ii;
-            ASSERT(diminuto_bits_mask(int16_t, ii) == (mm & 0xffff));
-            ASSERT(diminuto_bits_mask(int16_t, ii * diminuto_widthof(int16_t)) == 1);
+            ASSERT(diminuto_bits_or(int16_t, ii) == (mm & 0xffff));
+            ASSERT(diminuto_bits_or(int16_t, ii * diminuto_widthof(int16_t)) == 1);
         }
 
         for (ii = 0; ii < diminuto_widthof(int32_t); ++ii) {
             int32_t mm = (int32_t)1 << ii;
-            ASSERT(diminuto_bits_mask(int32_t, ii) == (mm & 0xffffffff));
-            ASSERT(diminuto_bits_mask(int32_t, ii * diminuto_widthof(int32_t)) == 1);
+            ASSERT(diminuto_bits_or(int32_t, ii) == (mm & 0xffffffff));
+            ASSERT(diminuto_bits_or(int32_t, ii * diminuto_widthof(int32_t)) == 1);
         }
 
         for (ii = 0; ii < diminuto_widthof(int64_t); ++ii) {
             int64_t mm = (int64_t)1 << ii;
-            ASSERT(diminuto_bits_mask(int64_t, ii) == (mm & 0xffffffffffffffffLL));
-            ASSERT(diminuto_bits_mask(int64_t, ii * diminuto_widthof(int64_t)) == 1);
+            ASSERT(diminuto_bits_or(int64_t, ii) == (mm & 0xffffffffffffffffLL));
+            ASSERT(diminuto_bits_or(int64_t, ii * diminuto_widthof(int64_t)) == 1);
         }
 
         STATUS();
     }
+
+    {
+        int ii;
+
+        TEST();
+
+        for (ii = 0; ii < diminuto_widthof(uint8_t); ++ii) {
+            uint8_t mm = ~((uint8_t)1 << ii);
+            ASSERT(diminuto_bits_and(uint8_t, ii) == mm);
+            ASSERT(diminuto_bits_and(uint8_t, ii * diminuto_widthof(uint8_t)) == 0xfeU);
+        }
+
+        for (ii = 0; ii < diminuto_widthof(uint16_t); ++ii) {
+            uint16_t mm = ~((uint16_t)1 << ii);
+            ASSERT(diminuto_bits_and(uint16_t, ii) == mm);
+            ASSERT(diminuto_bits_and(uint16_t, ii * diminuto_widthof(uint16_t)) == 0xfffeU);
+        }
+
+        for (ii = 0; ii < diminuto_widthof(uint32_t); ++ii) {
+            uint32_t mm = ~((uint32_t)1 << ii);
+            ASSERT(diminuto_bits_and(uint32_t, ii) == mm);
+            ASSERT(diminuto_bits_and(uint32_t, ii * diminuto_widthof(uint32_t)) == 0xfffffffeU);
+        }
+
+        for (ii = 0; ii < diminuto_widthof(uint64_t); ++ii) {
+            uint64_t mm = ~((uint64_t)1 << ii);
+            ASSERT(diminuto_bits_and(uint64_t, ii) == mm);
+            ASSERT(diminuto_bits_and(uint64_t, ii * diminuto_widthof(uint64_t)) == 0xfffffffffffffffeULL);
+        }
+
+        for (ii = 0; ii < diminuto_widthof(int8_t); ++ii) {
+            int8_t mm = ~((int8_t)1 << ii);
+            ASSERT(diminuto_bits_and(int8_t, ii) == (mm & 0xff));
+            ASSERT(diminuto_bits_and(int8_t, ii * diminuto_widthof(int8_t)) == 0xfe);
+        }
+
+        for (ii = 0; ii < diminuto_widthof(int16_t); ++ii) {
+            int16_t mm = ~((int16_t)1 << ii);
+            ASSERT(diminuto_bits_and(int16_t, ii) == (mm & 0xffff));
+            ASSERT(diminuto_bits_and(int16_t, ii * diminuto_widthof(int16_t)) == 0xfffe);
+        }
+
+        for (ii = 0; ii < diminuto_widthof(int32_t); ++ii) {
+            int32_t mm = ~((int32_t)1 << ii);
+            ASSERT(diminuto_bits_and(int32_t, ii) == (mm & 0xffffffff));
+            ASSERT(diminuto_bits_and(int32_t, ii * diminuto_widthof(int32_t)) == 0xfffffffe);
+        }
+
+        for (ii = 0; ii < diminuto_widthof(int64_t); ++ii) {
+            int64_t mm = ~((int64_t)1 << ii);
+            ASSERT(diminuto_bits_and(int64_t, ii) == (mm & 0xffffffffffffffffLL));
+            ASSERT(diminuto_bits_and(int64_t, ii * diminuto_widthof(int64_t)) == 0xfffffffffffffffeLL);
+        }
+
+        STATUS();
+    }
+
+#if 0
+    {
+        uint32_t array[3] = { 0, };
+        int ii;
+
+        TEST();
+
+        for (ii = 0; ii < (diminuto_widthof(array[0]) * diminuto_countof(array)); ii += 1) {
+            ASSERT(diminuto_bits_get(uint32_t, ii, array) == 0);
+        }
+
+        for (ii = 0; ii < (diminuto_widthof(array[0]) * diminuto_countof(array)); ii += 2) {
+            ASSERT(diminuto_bits_Set(uint32_t, ii, array) == 0);
+        }
+
+        STATUS();
+    }
+#endif
 
     EXIT();
 }
