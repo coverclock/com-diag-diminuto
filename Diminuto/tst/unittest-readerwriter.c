9411da85769a74ddada04933d1af358875991da4
Chip Overclock <coverclock@diag.com>
2021-01-07 14:13:03 -0700

    [1] Added some impatient readers and writers to final test sequence. PASSED [1.5] Took some tries to get state ring size right.

--- Diminuto/tst/unittest-readerwriter.c
+++ Diminuto/tst/unittest-readerwriter.c
@@ -64,6 +64,7 @@ struct Context {
     int identifier;
     diminuto_readerwriter_t * rwp;
     diminuto_ticks_t latency;
+    diminuto_ticks_t timeout;
     diminuto_ticks_t workload;
     int iterations;
 };
@@ -131,6 +132,74 @@ static void * writer(void * vp)
     }
 
     return (void *)(intptr_t)success;
+}
+
+static void * impatientreader(void * vp)
+{
+    static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    struct Context * cp = (struct Context *)vp;
+    int ii;
+    int success;
+
+    CHECKPOINT("impatientreader[%d] rwp=%p latency=%llu timeout=%llu workload=%llu iterations=%d\n", cp->identifier, cp->rwp, (unsigned long long)(cp->latency), (unsigned long long)(cp->timeout), (unsigned long long)(cp->workload), cp->iterations);
+
+    for (ii = 0; ii < cp->iterations; ++ii) {
+        success = 0;
+        diminuto_delay(cp->latency, 0);
+        if (diminuto_reader_begin_timed(cp->rwp, cp->timeout) == 0) {
+            pthread_cleanup_push(diminuto_reader_cleanup, cp->rwp);
+            DIMINUTO_CRITICAL_SECTION_BEGIN(&mutex);
+                ++readers;
+            DIMINUTO_CRITICAL_SECTION_END;
+            success = ((readers > 0) && (writers == 0));
+            CHECKPOINT("impatientreader[%d] readers=%d writers=%d success=%d\n", cp->identifier, readers, writers, success);
+            if (success) {
+                diminuto_delay(cp->workload, 0);
+            }
+            DIMINUTO_CRITICAL_SECTION_BEGIN(&mutex);
+                readers--;
+            DIMINUTO_CRITICAL_SECTION_END;
+            pthread_cleanup_pop(!0);
+        } else {
+            CHECKPOINT("impatientreader[%d] readers=%d writers=%d success=%d\n", cp->identifier, readers, writers, success);
+        }
+    }
+
+    return (void *)(intptr_t)(!0);
+}
+
+static void * impatientwriter(void * vp)
+{
+    static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    struct Context * cp = (struct Context *)vp;
+    int ii;
+    int success;
+
+    CHECKPOINT("impatientwriter[%d] rwp=%p latency=%llu timeout=%llu workload=%llu iterations=%d\n", cp->identifier, cp->rwp, (unsigned long long)(cp->latency), (unsigned long long)(cp->timeout), (unsigned long long)(cp->workload), cp->iterations);
+
+    for (ii = 0; ii < cp->iterations; ++ii) {
+        success = 0;
+        diminuto_delay(cp->latency, 0);
+        if (diminuto_writer_begin_timed(cp->rwp, cp->timeout) == 0) {
+            pthread_cleanup_push(diminuto_writer_cleanup, cp->rwp);
+            DIMINUTO_CRITICAL_SECTION_BEGIN(&mutex);
+                ++writers;
+            DIMINUTO_CRITICAL_SECTION_END;
+            success = ((readers == 0) && (writers == 1));
+            CHECKPOINT("impatientwriter[%d] readers=%d writers=%d success=%d\n", cp->identifier, readers, writers, success);
+            if (success) {
+                diminuto_delay(cp->workload, 0);
+            }
+            DIMINUTO_CRITICAL_SECTION_BEGIN(&mutex);
+                writers--;
+            DIMINUTO_CRITICAL_SECTION_END;
+            pthread_cleanup_pop(!0);
+        } else {
+            CHECKPOINT("impatientwriter[%d] readers=%d writers=%d success=%d\n", cp->identifier, readers, writers, success);
+        }
+    }
+
+    return (void *)(intptr_t)(!0);
 }
 
 /******************************************************************************/
@@ -753,18 +822,21 @@ int main(int argc, char * argv[])
         reading[0].identifier = 0;
         reading[0].rwp = &rw;
         reading[0].latency = frequency * 0;
+        reading[0].timeout = DIMINUTO_READERWRITER_INFINITY;
         reading[0].workload = frequency * 3;
         reading[0].iterations = 11;
 
         reading[1].identifier = 1;
         reading[1].rwp = &rw;
         reading[1].latency = frequency * 5;
+        reading[1].timeout = DIMINUTO_READERWRITER_INFINITY;
         reading[1].workload = frequency * 7;
         reading[1].iterations = 11;
 
         reading[2].identifier = 2;
         reading[2].rwp = &rw;
         reading[2].latency = frequency * 1;
+        reading[2].timeout = DIMINUTO_READERWRITER_INFINITY;
         reading[2].workload = frequency * 2;
         reading[2].iterations = 11;
 
@@ -815,18 +887,21 @@ int main(int argc, char * argv[])
         writing[0].identifier = 0;
         writing[0].rwp = &rw;
         writing[0].latency = frequency * 0;
+        writing[0].timeout = DIMINUTO_READERWRITER_INFINITY;
         writing[0].workload = frequency * 3;
         writing[0].iterations = 11;
 
         writing[1].identifier = 1;
         writing[1].rwp = &rw;
         writing[1].latency = frequency * 5;
+        writing[1].timeout = DIMINUTO_READERWRITER_INFINITY;
         writing[1].workload = frequency * 7;
         writing[1].iterations = 11;
 
         writing[2].identifier = 2;
         writing[2].rwp = &rw;
         writing[2].latency = frequency * 1;
+        writing[2].timeout = DIMINUTO_READERWRITER_INFINITY;
         writing[2].workload = frequency * 2;
         writing[2].iterations = 11;
 
@@ -880,18 +955,21 @@ int main(int argc, char * argv[])
         reading[0].identifier = 0;
         reading[0].rwp = &rw;
         reading[0].latency = frequency * 0;
+        reading[0].timeout = DIMINUTO_READERWRITER_INFINITY;
         reading[0].workload = frequency * 3;
         reading[0].iterations = 11;
 
         reading[1].identifier = 1;
         reading[1].rwp = &rw;
         reading[1].latency = frequency * 5;
+        reading[1].timeout = DIMINUTO_READERWRITER_INFINITY;
         reading[1].workload = frequency * 7;
         reading[1].iterations = 11;
 
         reading[2].identifier = 2;
         reading[2].rwp = &rw;
         reading[2].latency = frequency * 1;
+        reading[2].timeout = DIMINUTO_READERWRITER_INFINITY;
         reading[2].workload = frequency * 2;
         reading[2].iterations = 11;
 
@@ -902,18 +980,21 @@ int main(int argc, char * argv[])
         writing[0].identifier = 3;
         writing[0].rwp = &rw;
         writing[0].latency = frequency * 0;
+        writing[0].timeout = DIMINUTO_READERWRITER_INFINITY;
         writing[0].workload = frequency * 3;
         writing[0].iterations = 11;
 
         writing[1].identifier = 4;
         writing[1].rwp = &rw;
         writing[1].latency = frequency * 5;
+        writing[1].timeout = DIMINUTO_READERWRITER_INFINITY;
         writing[1].workload = frequency * 7;
         writing[1].iterations = 11;
 
         writing[2].identifier = 5;
         writing[2].rwp = &rw;
         writing[2].latency = frequency * 1;
+        writing[2].timeout = DIMINUTO_READERWRITER_INFINITY;
         writing[2].workload = frequency * 2;
         writing[2].iterations = 11;
 
@@ -971,13 +1052,32 @@ int main(int argc, char * argv[])
     {
         diminuto_thread_t readers[64];
         diminuto_thread_t writers[8];
+        diminuto_thread_t impatientreaders[4];
+        diminuto_thread_t impatientwriters[2];
         struct Context reading[diminuto_countof(readers)];
         struct Context writing[diminuto_countof(writers)];
-        void * reads[countof(readers)];
-        void * writes[countof(writers)];
-        diminuto_readerwriter_state_t state[diminuto_countof(readers) + diminuto_countof(writers)];
+        struct Context impatientreading[diminuto_countof(impatientreaders)];
+        struct Context impatientwriting[diminuto_countof(impatientwriters)];
+        void * reads[diminuto_countof(readers)];
+        void * writes[diminuto_countof(writers)];
+        void * impatientreads[diminuto_countof(impatientreaders)];
+        void * impatientwrites[diminuto_countof(impatientwriters)];
+        static const int MAXIMUM = 64;
+        static const int TOTAL = 64 + 8 + 4 + 2;
+        static const int EXTRA = 100;
+        /*
+         * When allowing for failures that place IGNORE tokens in the
+         * state ring, it can be difficult to calcualte how big the
+         * ring needs to be. It all depends on how many times the reader
+         * or writer can retry before its IGNORE tokens are removed from
+         * the head of the ring by it or another reader or writer. The
+         * number below is just a SWAG based on my own testing. Because
+         * the parameters for each reader and writer below are deliberately
+         * random, this number might be too low. You'll know it's too low
+         * when you start seeing NOSPC ("No space left on device") errors.
+         */
+        diminuto_readerwriter_state_t state[TOTAL + EXTRA];
         diminuto_readerwriter_t rw;
-        int maximum = (diminuto_countof(readers) > diminuto_countof(writers) ? diminuto_countof(readers) : diminuto_countof(writers));
         int ii;
 
         TEST();
@@ -990,6 +1090,7 @@ int main(int argc, char * argv[])
             reading[ii].identifier = ii;
             reading[ii].rwp = &rw;
             reading[ii].latency = frequency * randy(0, 3);
+            reading[ii].timeout = DIMINUTO_READERWRITER_INFINITY;
             reading[ii].workload = frequency * randy(1, 5);
             reading[ii].iterations = randy(2, 13);
             reads[ii] = (void *)0;
@@ -1000,22 +1101,51 @@ int main(int argc, char * argv[])
             writing[ii].identifier = ii;
             writing[ii].rwp = &rw;
             writing[ii].latency = frequency * randy(0, 3);
+            writing[ii].timeout = DIMINUTO_READERWRITER_INFINITY;
             writing[ii].workload = frequency * randy(1, 5);
             writing[ii].iterations = randy(2, 13);
             writes[ii] = (void *)0;
             ASSERT(diminuto_thread_init(&writers[ii], writer) == &writers[ii]);
         }
 
-        for (ii = 0; ii < maximum; ++ii) {
+        for (ii = 0; ii < diminuto_countof(impatientreaders); ++ii) {
+            impatientreading[ii].identifier = ii;
+            impatientreading[ii].rwp = &rw;
+            impatientreading[ii].latency = frequency * randy(0, 3);
+            impatientreading[ii].timeout = (ii == 0) ? 0 : frequency * randy(1, 7);;
+            impatientreading[ii].workload = frequency * randy(1, 5);
+            impatientreading[ii].iterations = randy(2, 13);
+            impatientreads[ii] = (void *)0;
+            ASSERT(diminuto_thread_init(&impatientreaders[ii], impatientreader) == &impatientreaders[ii]);
+        }
+
+        for (ii = 0; ii < diminuto_countof(impatientwriters); ++ii) {
+            impatientwriting[ii].identifier = ii;
+            impatientwriting[ii].rwp = &rw;
+            impatientwriting[ii].latency = frequency * randy(0, 3);
+            impatientwriting[ii].timeout = (ii == 0) ? 0 : frequency * randy(1, 7);;
+            impatientwriting[ii].workload = frequency * randy(1, 5);
+            impatientwriting[ii].iterations = randy(2, 13);
+            impatientwrites[ii] = (void *)0;
+            ASSERT(diminuto_thread_init(&impatientwriters[ii], impatientwriter) == &impatientwriters[ii]);
+        }
+
+        for (ii = 0; ii < MAXIMUM; ++ii) {
             if (ii < diminuto_countof(readers)) {
                 ASSERT(diminuto_thread_start(&readers[ii], &reading[ii]) == 0);
             }
             if (ii < diminuto_countof(writers)) {
                 ASSERT(diminuto_thread_start(&writers[ii], &writing[ii]) == 0);
             }
+            if (ii < diminuto_countof(impatientreaders)) {
+                ASSERT(diminuto_thread_start(&impatientreaders[ii], &impatientreading[ii]) == 0);
+            }
+            if (ii < diminuto_countof(impatientwriters)) {
+                ASSERT(diminuto_thread_start(&impatientwriters[ii], &impatientwriting[ii]) == 0);
+            }
         }
 
-        for (ii = 0; ii < maximum; ++ii) {
+        for (ii = 0; ii < MAXIMUM; ++ii) {
             if (ii < diminuto_countof(readers)) {
                 ASSERT(diminuto_thread_join(&readers[ii], &reads[ii]) == 0);
                 CHECKPOINT("reads[%d]=%d\n", ii, (int)(intptr_t)reads[ii]);
@@ -1024,14 +1154,28 @@ int main(int argc, char * argv[])
                 ASSERT(diminuto_thread_join(&writers[ii], &writes[ii]) == 0);
                 CHECKPOINT("writes[%d]=%d\n", ii, (int)(intptr_t)writes[ii]);
             }
+            if (ii < diminuto_countof(impatientreaders)) {
+                ASSERT(diminuto_thread_join(&impatientreaders[ii], &impatientreads[ii]) == 0);
+                CHECKPOINT("impatientreads[%d]=%d\n", ii, (int)(intptr_t)impatientreads[ii]);
+            }
+            if (ii < diminuto_countof(impatientwriters)) {
+                ASSERT(diminuto_thread_join(&impatientwriters[ii], &impatientwrites[ii]) == 0);
+                CHECKPOINT("impatientwrites[%d]=%d\n", ii, (int)(intptr_t)impatientwrites[ii]);
+            }
         }
 
-        for (ii = 0; ii < maximum; ++ii) {
+        for (ii = 0; ii < MAXIMUM; ++ii) {
             if (ii < diminuto_countof(readers)) {
                 ASSERT(((intptr_t)reads[ii]) != 0);
             }
             if (ii < diminuto_countof(writers)) {
                 ASSERT(((intptr_t)writes[ii]) != 0);
+            }
+            if (ii < diminuto_countof(impatientreaders)) {
+                ASSERT(((intptr_t)impatientreads[ii]) != 0);
+            }
+            if (ii < diminuto_countof(impatientwriters)) {
+                ASSERT(((intptr_t)impatientwrites[ii]) != 0);
             }
         }
 
