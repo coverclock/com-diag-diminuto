efa58025747c44cc5bc46f17f37203430a00bf73
Chip Overclock <coverclock@diag.com>
2021-03-19 11:15:14 -0600

    [1] Mainlining scatter/gather: compiles.

---
+++ Diminuto/inc/com/diag/diminuto/diminuto_scattergather.h
@@ -0,0 +1,243 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+#ifndef _H_COM_DIAG_DIMINUTO_SCATTERGATHER_
+#define _H_COM_DIAG_DIMINUTO_SCATTERGATHER_
+/**
+ * @file
+ * @copyright Copyright 2021 Digital Aggregates Corporation, Colorado, USA.
+ * @note Licensed under the terms in LICENSE.txt.
+ * @brief This defines the Diminuto IPC Scatter/Gather API.
+ * @author Chip Overclock <mailto:coverclock@diag.com>
+ * @see Diminuto <https://github.com/coverclock/com-diag-diminuto>
+ * @details
+ * The Scatter/Gather makes use of Diminuto Lists with the vector I/O
+ * system calls to decrease the amount of data copying necessary when
+ * consolidating a bunch of fields into a single contiguous data packet
+ * for sending/writing, or distributing a single contiguous data packet
+ * that is received/read into multiple fields. Scatter/Gather can be
+ * used for interprocess communication (IPC) or for file-ish reading
+ * and writing.
+ *
+ * SEE ALSO
+ *
+ * tst/unittest-ipc-ancillary.c
+ *
+ * REFERENCES
+ *
+ * unix(7) man page
+ *
+ * socket(7) man page
+ *
+ * sendmsg(2) man page
+ *
+ * recvmsg(2) man page
+ *
+ * cmsg(3) man page
+ *
+ * readv(2) man page
+ *
+ * writev(2) man page
+ *
+ * POSIX 1003.1-2001 (readv, writev)
+ *
+ * Single UNIX Specification version 4 (POSIX:2008), Issue 7 (sendmsg, recvmsg)
+ *
+ * <linux/uio.h> 5.4.0: UIO_MAXIOV=1024 UIO_FASTIOV=8
+ *
+ * POSIX 1003.1g 5.4.1.1: "UIO_MAXIOV shall be at least 16"
+ */
+
+#include "com/diag/diminuto/diminuto_list.h"
+#include "com/diag/diminuto/diminuto_buffer.h"
+#include "com/diag/diminuto/diminuto_containerof.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+#include <stdint.h>
+#include <stdio.h>
+
+/*******************************************************************************
+ * CONSTANTS
+ ******************************************************************************/
+
+enum {
+    DIMINUTO_SCATTERGATHER_NODES = UIO_MAXIOV,     /* Overkill. */
+    DIMINUTO_SCATTERGATHER_VECTOR = UIO_MAXIOV,    /* Maximum. */
+};
+
+/*******************************************************************************
+ * TYPES
+ ******************************************************************************/
+
+typedef struct DiminutoScatterGatherBuffer {
+    /*
+     * The type of the length field is way overkill as is uint32_t
+     * (although uint16_t is too small). But it insures the caller
+     * of the alignment. uint64_t will be double the size of size_t
+     * on some ARM platforms (especially those with 32-bit kernels)
+     * but the same as size_t on 64-bit ARMs and all x86_64s.
+     */
+    uint64_t length; /* This is the data length, not the buffer length. */
+    /*
+     * Insure that the payload portion of the buffer is 8-byte aligned.
+     * This guarantees that the caller can use the payload field to
+     * store anything requiring 8-byte alignment or less.
+     */
+    uint64_t payload[0]; /* This will cause -pendantic warnings. */
+} diminuto_scattergather_buffer_t;
+
+/*
+ * Everything other than a Buffer is a List node. But the different
+ * type names help me keep straight the role that the List node is
+ * serving in the API. Don't mix them up, otherwise wackiness will
+ * ensue.
+ */
+
+typedef diminuto_list_t diminuto_scattergather_pool_t;
+
+typedef diminuto_list_t diminuto_scattergather_segment_t;
+
+typedef diminuto_list_t diminuto_scattergather_record_t;
+
+/*******************************************************************************
+ * GENERATORS
+ ******************************************************************************/
+
+#define DIMINUTO_SCATTERGATHER_POOL_INIT(_POINTER_) DIMINUTO_LIST_NULLINIT(_POINTER_)
+
+#define DIMINUTO_SCATTERGATHER_SEGMENT_INIT(_POINTER_) DIMINUTO_LIST_NULLINIT(_POINTER_)
+
+#define DIMINUTO_SCATTERGATHER_RECORD_INIT(_POINTER_) DIMINUTO_LIST_NULLINIT(_POINTER_)
+
+#define diminuto_scattergather_bufferof(_PAYLOAD_) diminuto_containerof(diminuto_scattergather_buffer_t, payload, (_PAYLOAD_))
+
+/*******************************************************************************
+ * Pool
+ ******************************************************************************/
+
+static inline diminuto_scattergather_pool_t * diminuto_scattergather_pool_init(diminuto_scattergather_pool_t * pp) {
+    return diminuto_list_nullinit(pp);
+}
+
+extern void diminuto_scattergather_pool_fini(diminuto_scattergather_pool_t * pp);
+
+extern diminuto_list_t * diminuto_scattergather_pool_get(diminuto_scattergather_pool_t * pp);
+
+extern void diminuto_scattergather_pool_put(diminuto_scattergather_pool_t * pp, diminuto_list_t * np);
+
+extern diminuto_scattergather_pool_t * diminuto_scattergather_pool_populate(diminuto_scattergather_pool_t * pp, diminuto_scattergather_segment_t sa[], size_t sn);
+
+/*******************************************************************************
+ * Segment
+ ******************************************************************************/
+
+static inline diminuto_scattergather_segment_t * diminuto_scattergather_segment_init(diminuto_scattergather_segment_t * sp) {
+    return diminuto_list_nullinit(sp);
+}
+
+static inline diminuto_scattergather_segment_t * diminuto_scattergather_segment_fini(diminuto_scattergather_segment_t * sp) {
+    return diminuto_list_dataset(diminuto_list_remove(sp), (void *)0);
+}
+
+/*
+ * The address returned by this function will be aligned on at least
+ * an eight-byte boundary.
+ */
+static inline void * diminuto_scattergather_segment_payload_get(diminuto_scattergather_segment_t * sp) {
+    return (void *)(&(((diminuto_scattergather_buffer_t *)diminuto_list_data(sp))->payload[0]));
+}
+
+static inline size_t diminuto_scattergather_segment_length_get(diminuto_scattergather_segment_t * sp) {
+    return ((diminuto_scattergather_buffer_t *)diminuto_list_data(sp))->length;
+}
+
+static inline size_t diminuto_scattergather_segment_length_set(diminuto_scattergather_segment_t * sp, size_t ll) {
+    return ((diminuto_scattergather_buffer_t *)diminuto_list_data(sp))->length = ll;
+}
+
+extern diminuto_scattergather_segment_t * diminuto_scattergather_segment_allocate(diminuto_scattergather_pool_t * pp, size_t size);
+
+static inline void diminuto_scattergather_segment_free(diminuto_scattergather_pool_t * pp, diminuto_scattergather_segment_t * sp)
+{
+    diminuto_buffer_free(diminuto_list_data(sp));
+    (void)diminuto_scattergather_pool_put(pp, diminuto_scattergather_segment_fini(sp));
+}
+
+/*******************************************************************************
+ * Record
+ ******************************************************************************/
+
+static inline diminuto_scattergather_record_t * diminuto_scattergather_record_init(diminuto_scattergather_record_t * rp) {
+    return diminuto_list_nullinit(rp);
+}
+
+static inline diminuto_scattergather_record_t * diminuto_scattergather_record_fini(diminuto_scattergather_record_t * rp) {
+    return diminuto_list_dataset(diminuto_list_remove(rp), (void *)0);
+}
+
+static inline diminuto_scattergather_segment_t * diminuto_scattergather_record_segment_remove(diminuto_scattergather_record_t * rp /* Unused. */, diminuto_scattergather_segment_t * sp) {
+    return diminuto_list_remove(sp);
+}
+
+static inline diminuto_scattergather_segment_t * diminuto_scattergather_record_segment_prepend(diminuto_scattergather_record_t * rp, diminuto_scattergather_segment_t * sp) {
+    return diminuto_list_push(rp, sp);
+}
+
+static inline diminuto_scattergather_segment_t * diminuto_scattergather_record_segment_append(diminuto_scattergather_record_t * rp, diminuto_scattergather_segment_t * sp) {
+    return diminuto_list_enqueue(rp, sp);
+}
+
+static inline diminuto_scattergather_segment_t * diminuto_scattergather_record_segment_insert(diminuto_scattergather_segment_t * op, diminuto_scattergather_segment_t * sp) {
+    return diminuto_list_splice(op, sp);
+}
+
+static inline diminuto_scattergather_segment_t * diminuto_scattergather_record_segment_replace(diminuto_scattergather_segment_t * op, diminuto_scattergather_segment_t * sp) {
+    return diminuto_list_replace(op, sp);
+}
+
+static inline diminuto_scattergather_segment_t * diminuto_scattergather_record_segment_head(diminuto_scattergather_record_t * rp) {
+    return diminuto_list_head(rp);
+}
+
+static inline diminuto_scattergather_segment_t * diminuto_scattergather_record_segment_tail(diminuto_scattergather_record_t * rp) {
+    return diminuto_list_tail(rp);
+}
+
+static inline diminuto_scattergather_segment_t * diminuto_scattergather_record_segment_next(diminuto_scattergather_record_t * rp, diminuto_scattergather_segment_t * sp) {
+    return (diminuto_list_next(sp) == rp) ? (diminuto_scattergather_segment_t *)0 : diminuto_list_next(sp);
+}
+
+extern size_t diminuto_scattergather_record_enumerate(diminuto_scattergather_record_t * rp);
+
+extern size_t diminuto_scattergather_record_measure(diminuto_scattergather_record_t * rp);
+
+extern diminuto_scattergather_record_t * diminuto_scattergather_record_dump(FILE * fp, diminuto_scattergather_record_t * rp);
+
+extern struct iovec * diminuto_scattergather_record_vectorize(diminuto_scattergather_record_t * rp, struct iovec va[], size_t nn);
+
+static inline diminuto_scattergather_record_t * diminuto_scattergather_record_segment_free(diminuto_scattergather_pool_t * pp, diminuto_scattergather_record_t * rp, diminuto_scattergather_segment_t * sp)
+{
+    (void)diminuto_scattergather_segment_free(pp, diminuto_scattergather_record_segment_remove(rp, sp));
+    return rp;
+}
+
+extern diminuto_scattergather_record_t * diminuto_scattergather_record_segments_free(diminuto_scattergather_pool_t * pp, diminuto_scattergather_record_t * rp);
+
+extern diminuto_scattergather_record_t * diminuto_scattergather_record_allocate(diminuto_scattergather_pool_t * pp);
+
+static inline void diminuto_scattergather_record_free(diminuto_scattergather_pool_t * pp, diminuto_scattergather_record_t * rp) {
+    diminuto_scattergather_pool_put(pp, diminuto_scattergather_record_fini(diminuto_scattergather_record_segments_free(pp, rp)));
+}
+
+/*******************************************************************************
+ * INPUT/OUTPUT
+ ******************************************************************************/
+
+extern ssize_t diminuto_scattergather_record_write(int fd, diminuto_scattergather_record_t * rp);
+
+extern ssize_t diminuto_scattergather_record_read(int fd, diminuto_scattergather_record_t * rp);
+
+extern ssize_t diminuto_scattergather_record_send(int fd, diminuto_scattergather_record_t * rp, diminuto_ipv4_t address, diminuto_port_t port);
+
+extern ssize_t diminuto_scattergather_record_receive(int fd, diminuto_scattergather_record_t * rp);
+
+#endif
---
+++ Diminuto/src/diminuto_scattergather.c
@@ -0,0 +1,325 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+/**
+ * @file
+ * @copyright Copyright 2021 Digital Aggregates Corporation, Colorado, USA.
+ * @note Licensed under the terms in LICENSE.txt.
+ * @brief This implements the Diminuto IPC Scatter/Gather feature.
+ * @author Chip Overclock <mailto:coverclock@diag.com>
+ * @see Diminuto <https://github.com/coverclock/com-diag-diminuto>
+ * @details
+ */
+
+#include "com/diag/diminuto/diminuto_scattergather.h"
+#include "com/diag/diminuto/diminuto_criticalsection.h"
+#include "com/diag/diminuto/diminuto_countof.h"
+#include "com/diag/diminuto/diminuto_log.h"
+#include "com/diag/diminuto/diminuto_dump.h"
+#include "com/diag/diminuto/diminuto_ipc4.h"
+#include "com/diag/diminuto/diminuto_fletcher.h"
+#include "com/diag/diminuto/diminuto_reaper.h"
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+/*******************************************************************************
+ * GLOBALS
+ ******************************************************************************/
+
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+/*******************************************************************************
+ * Pool
+ ******************************************************************************/
+
+void diminuto_scattergather_pool_fini(diminuto_scattergather_pool_t * pp)
+{
+    diminuto_list_t * np = (diminuto_list_t *)0;
+
+    while ((np = diminuto_list_head(pp)) != (diminuto_list_t *)0) {
+        (void)diminuto_list_remove(np);
+    }
+}
+
+diminuto_list_t * diminuto_scattergather_pool_get(diminuto_scattergather_pool_t * pp)
+{
+    diminuto_list_t * np = (diminuto_list_t *)0;
+
+    DIMINUTO_CRITICAL_SECTION_BEGIN(&mutex);
+        np = diminuto_list_dequeue(pp);
+    DIMINUTO_CRITICAL_SECTION_END;
+
+    return np;
+}
+
+void diminuto_scattergather_pool_put(diminuto_scattergather_pool_t * pp, diminuto_list_t * np)
+{
+    DIMINUTO_CRITICAL_SECTION_BEGIN(&mutex);
+        (void)diminuto_list_enqueue(pp, np);
+    DIMINUTO_CRITICAL_SECTION_END;
+}
+
+diminuto_scattergather_pool_t * diminuto_scattergather_pool_populate(diminuto_scattergather_pool_t * pp, diminuto_scattergather_segment_t sa[], size_t sn)
+{
+    int ii = 0;
+
+    DIMINUTO_CRITICAL_SECTION_BEGIN(&mutex);
+        for (ii = 0; ii < sn; ++ii) {
+            (void)diminuto_list_enqueue(pp, diminuto_list_nullinit(&sa[ii]));
+        }
+    DIMINUTO_CRITICAL_SECTION_END;
+
+    return pp;
+}
+
+/*******************************************************************************
+ * Segment
+ ******************************************************************************/
+
+diminuto_scattergather_segment_t * diminuto_scattergather_segment_allocate(diminuto_scattergather_pool_t * pp, size_t size)
+{
+    diminuto_scattergather_buffer_t * bp = (diminuto_scattergather_buffer_t *)0;
+    diminuto_scattergather_segment_t * sp = (diminuto_scattergather_segment_t *)0;
+
+    /*
+     * This function returns a buffer whose beginning address is
+     * aligned on at least an eight-byte boundary. The header we
+     * put before the payload portion is eight bytes long.
+     */
+    if ((bp = (diminuto_scattergather_buffer_t *)diminuto_buffer_malloc(sizeof(diminuto_scattergather_buffer_t) + size)) == (void *)0) {
+        /* Do nothing. */
+    } else if ((sp = diminuto_scattergather_pool_get(pp)) == (diminuto_scattergather_segment_t *)0) {
+        diminuto_buffer_free(bp);
+        errno = ENOMEM;
+        diminuto_perror("diminuto_scattergather_segment_allocate");
+    } else {
+        /*
+         * The caller can always change this length, but most of the time
+         * it is allocating a segment for a fixed length field or header
+         * and this will save a lot of time and code.
+         */
+        bp->length = size;
+        diminuto_list_dataset(sp, bp);
+    }
+
+    return sp;
+}
+
+/*******************************************************************************
+ * Record
+ ******************************************************************************/
+
+size_t diminuto_scattergather_record_enumerate(diminuto_scattergather_record_t * rp)
+{
+    size_t nn = 0;
+    diminuto_scattergather_segment_t * sp = (diminuto_scattergather_segment_t *)0;
+
+    for (sp = diminuto_scattergather_record_segment_head(rp); sp != (diminuto_scattergather_segment_t *)0; sp = diminuto_scattergather_record_segment_next(rp, sp)) {
+        nn += 1;
+    }
+
+    return nn;    
+}
+
+size_t diminuto_scattergather_record_measure(diminuto_scattergather_record_t * rp)
+{
+    size_t ll = 0;
+    diminuto_scattergather_segment_t * sp = (diminuto_scattergather_segment_t *)0;
+
+    for (sp = diminuto_scattergather_record_segment_head(rp); sp != (diminuto_scattergather_segment_t *)0; sp = diminuto_scattergather_record_segment_next(rp, sp)) {
+        if (diminuto_list_data(sp) != (void *)0) {
+            ll += diminuto_scattergather_segment_length_get(sp);
+        }
+    }
+
+    return ll;    
+}
+
+diminuto_scattergather_record_t * diminuto_scattergather_record_dump(FILE * fp, diminuto_scattergather_record_t * rp)
+{
+    diminuto_scattergather_segment_t * sp = (diminuto_scattergather_segment_t *)0;
+    void * pp = 0;
+    size_t ii = 0;
+    size_t ll = 0;
+    size_t tt = 0;
+
+    fprintf(fp, "RECORD %p:\n", rp);
+    for (sp = diminuto_scattergather_record_segment_head(rp); sp != (diminuto_scattergather_segment_t *)0; sp = diminuto_scattergather_record_segment_next(rp, sp)) {
+        fprintf(fp, "  SEGMENT %p: #%zu\n", sp, ii++);
+        pp = diminuto_scattergather_segment_payload_get(sp);
+        ll = diminuto_scattergather_segment_length_get(sp);
+        tt += ll;
+        fprintf(fp, "    PAYLOAD %p: [%zu]\n", pp, ll);
+        diminuto_dump_general(fp, pp, ll, 0, '.', 0, 0, 6);
+    }
+    fprintf(fp, "  TOTAL %p: #%zu [%zu]\n", rp, ii, tt);
+
+    return rp;    
+}
+
+struct iovec * diminuto_scattergather_record_vectorize(diminuto_scattergather_record_t * rp, struct iovec va[], size_t nn)
+{
+    diminuto_scattergather_segment_t * sp = (diminuto_scattergather_segment_t *)0;
+    size_t ii = 0;
+
+    for (sp = diminuto_scattergather_record_segment_head(rp); sp != (diminuto_scattergather_segment_t *)0; sp = diminuto_scattergather_record_segment_next(rp, sp)) {
+        if (ii < nn) {
+            va[ii].iov_base = diminuto_scattergather_segment_payload_get(sp);
+            va[ii].iov_len = diminuto_scattergather_segment_length_get(sp);
+            ii += 1;
+        } else {
+            /*
+             * It is not an error to provide a vector that
+             * is too short. It's probably a bug, but might
+             * be intentional. So we return an error, but
+             * we don't emit an error message.
+             */
+            errno = ETOOMANYREFS;
+            return (struct iovec *)0;
+        }
+    }
+
+    return va;    
+}
+
+diminuto_scattergather_record_t * diminuto_scattergather_record_segments_free(diminuto_scattergather_pool_t * pp, diminuto_scattergather_record_t * rp)
+{
+    diminuto_scattergather_segment_t * sp = (diminuto_scattergather_segment_t *)0;
+
+    while ((sp = diminuto_scattergather_record_segment_head(rp)) != (diminuto_scattergather_segment_t *)0) {
+        (void)diminuto_scattergather_record_segment_free(pp, rp, sp);
+    }
+
+    return rp;    
+}
+
+diminuto_scattergather_record_t * diminuto_scattergather_record_allocate(diminuto_scattergather_pool_t * pp)
+{
+    diminuto_scattergather_record_t * rp = (diminuto_scattergather_record_t *)0;
+
+    if ((rp = diminuto_scattergather_pool_get(pp)) == (diminuto_scattergather_record_t *)0) {
+        errno = ENOMEM;
+        diminuto_perror("diminuto_scattergather_record_allocate");
+    }
+
+    return rp;
+}
+
+/*******************************************************************************
+ * INPUT/OUTPUT
+ ******************************************************************************/
+
+ssize_t diminuto_scattergather_record_write(int fd, diminuto_scattergather_record_t * rp)
+{
+    ssize_t total = -1;
+    size_t nn = 0;
+    struct iovec * vp = (struct iovec *)0;
+
+    nn = diminuto_scattergather_record_enumerate(rp);
+
+    if (!((0 < nn) && (nn <= DIMINUTO_SCATTERGATHER_VECTOR))) {
+        errno = EINVAL;
+        diminuto_perror("diminuto_scattergather_record_write: enumeration");
+    } else if ((vp = (struct iovec *)alloca(nn * sizeof(*vp))) == (struct iovec *)0) {
+        diminuto_perror("diminuto_scattergather_record_write: alloca");
+    } else if (diminuto_scattergather_record_vectorize(rp, vp, nn) == (struct iovec *)0) {
+        diminuto_perror("diminuto_scattergather_record_write: diminuto_scattergather_record_vectorize");
+    } else if ((total = writev(fd, vp, nn)) < 0) {
+        diminuto_perror("diminuto_scattergather_record_write: writev");
+    } else {
+        /* Do nothing. */
+    }
+
+    return total;
+}
+
+ssize_t diminuto_scattergather_record_read(int fd, diminuto_scattergather_record_t * rp)
+{
+    ssize_t total = -1;
+    size_t nn = 0;
+    struct iovec * vp = (struct iovec *)0;
+
+    nn = diminuto_scattergather_record_enumerate(rp);
+
+    if (!((0 < nn) && (nn <= DIMINUTO_SCATTERGATHER_VECTOR))) {
+        errno = EINVAL;
+        diminuto_perror("diminuto_scattergather_record_read: enumeration");
+    } else if ((vp = (struct iovec *)alloca(nn * sizeof(*vp))) == (struct iovec *)0) {
+        diminuto_perror("diminuto_scattergather_record_write: alloca");
+    } else if (diminuto_scattergather_record_vectorize(rp, vp, nn) == (struct iovec *)0) {
+        diminuto_perror("diminuto_scattergather_record_read: diminuto_scattergather_record_vectorize");
+    } else if ((total = readv(fd, vp, nn)) < 0) {
+        diminuto_perror("diminuto_scattergather_record_write: writev");
+    } else {
+        /* Do nothing. */
+    }
+
+    return total;
+}
+
+ssize_t diminuto_scattergather_record_send(int fd, diminuto_scattergather_record_t * rp, diminuto_ipv4_t address, diminuto_port_t port)
+{
+    ssize_t total = 0;
+    struct iovec * vp = (struct iovec *)0;
+    struct msghdr message = { 0, };
+    struct sockaddr_in sa = { 0, };
+    struct sockaddr * sap = (struct sockaddr *)0;
+    socklen_t length = 0;
+
+    if (port > 0) {
+        length = sizeof(sa);
+        sa.sin_family = AF_INET;
+        sa.sin_addr.s_addr = htonl(address);
+        sa.sin_port = htons(port);
+        sap = (struct sockaddr *)&sa;
+    }
+
+    message.msg_name = sap;
+    message.msg_namelen = length;
+    message.msg_iovlen = diminuto_scattergather_record_enumerate(rp);
+
+    if (!((0 < message.msg_iovlen) && (message.msg_iovlen <= DIMINUTO_SCATTERGATHER_VECTOR))) {
+        errno = EINVAL;
+        diminuto_perror("diminuto_scattergather_record_send: enumeration");
+    } else if ((vp = (struct iovec *)alloca(message.msg_iovlen * sizeof(*message.msg_iov))) == (struct iovec *)0) {
+        diminuto_perror("diminuto_scattergather_record_send: alloca");
+    } else if ((message.msg_iov = diminuto_scattergather_record_vectorize(rp, vp, message.msg_iovlen)) == (struct iovec *)0) {
+        diminuto_perror("diminuto_scattergather_record_send: diminuto_scattergather_record_vectorize");
+    } else if ((total = sendmsg(fd, &message, 0)) < 0) {
+        diminuto_perror("diminuto_scattergather_record_send: sendmsg");
+    } else {
+        /* Do nothing. */
+    }
+
+    return total;
+}
+
+ssize_t diminuto_scattergather_record_receive(int fd, diminuto_scattergather_record_t * rp)
+{
+    ssize_t total = 0;
+    struct iovec * vp = (struct iovec *)0;
+    struct msghdr message = { 0, };
+
+    message.msg_name = (struct sockaddr *)0;
+    message.msg_namelen = 0;
+    message.msg_iovlen = diminuto_scattergather_record_enumerate(rp);
+
+    if (!((0 < message.msg_iovlen) && (message.msg_iovlen <= DIMINUTO_SCATTERGATHER_VECTOR))) {
+        errno = EINVAL;
+        diminuto_perror("diminuto_scattergather_record_receive: enumeration");
+    } else if ((vp = (struct iovec *)alloca(message.msg_iovlen * sizeof(*message.msg_iov))) == (struct iovec *)0) {
+        diminuto_perror("diminuto_scattergather_record_receive: alloca");
+    } else if ((message.msg_iov = diminuto_scattergather_record_vectorize(rp, vp, message.msg_iovlen)) == (struct iovec *)0) {
+        diminuto_perror("diminuto_scattergather_record_receive: diminuto_scattergather_record_vectorize");
+    } else if ((total = recvmsg(fd, &message, 0)) < 0) {
+        diminuto_perror("diminuto_scattergather_record_receive: sendmsg");
+    } else {
+        /* Do nothing. */
+    }
+
+    return total;
+}
