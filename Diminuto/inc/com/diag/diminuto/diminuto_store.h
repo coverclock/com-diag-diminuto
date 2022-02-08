0074706ae9704cd88857db041595a80fa563e014
Chip Overclock <coverclock@diag.com>
2022-02-07 17:23:54 -0700

    [1] Comments (copyrights).

--- Diminuto/inc/com/diag/diminuto/diminuto_store.h
+++ Diminuto/inc/com/diag/diminuto/diminuto_store.h
@@ -4,7 +4,7 @@
 
 /**
  * @file
- * @copyright Copyright 2014-2020 Digital Aggregates Corporation, Colorado, USA.
+ * @copyright Copyright 2014-2022 Digital Aggregates Corporation, Colorado, USA.
  * @note Licensed under the terms in LICENSE.txt.
  * @brief Implements a key-value memory store using a Red-Black tree.
  * @author Chip Overclock <mailto:coverclock@diag.com>
