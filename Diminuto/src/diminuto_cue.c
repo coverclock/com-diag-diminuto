d7757ca39e8ccab1c8273cf69f5602c04cb48462
Chip Overclock <coverclock@diag.com>
2022-05-08 09:17:49 -0600

    [1] Changed Cue from logical to bitwise operations. PASSED

--- Diminuto/src/diminuto_cue.c
+++ Diminuto/src/diminuto_cue.c
@@ -29,7 +29,7 @@ int diminuto_cue_debounce(diminuto_cue_state_t * statep, int input)
     statep->b = statep->a;
     statep->a = !!input;
     statep->p = statep->r;
-    return statep->r = (statep->p && (statep->a || statep->b || statep->c)) || (statep->a && statep->b && statep->c);
+    return statep->r = (statep->p & (statep->a | statep->b | statep->c)) | (statep->a & statep->b & statep->c);
 }
 
 diminuto_cue_edge_t diminuto_cue_edge(const diminuto_cue_state_t * statep)
