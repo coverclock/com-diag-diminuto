754486c9aa432e57853e27341b1a36cb82d9738b
Chip Overclock <coverclock@diag.com>
2022-10-04 21:28:43 +0800

    [1] Expanded Barrier unit test to examine RISC-V atomic instructions. TESTED

--- Diminuto/tst/unittest-barrier.c
+++ Diminuto/tst/unittest-barrier.c
@@ -8,25 +8,56 @@
  * @see Diminuto <https://github.com/coverclock/com-diag-diminuto>
  * @details
  * This is a unit test of the memory Barrier feature.
- * This is mostly just to insure it compiles.
- */
-
-#include "com/diag/diminuto/diminuto_unittest.h"
-#include "com/diag/diminuto/diminuto_barrier.h"
-
-/**
- * Example function whose inlines expand into GNU built-ins.
+ *
+ * I'm still pondering how to actually functionally test
+ * this code without some hardware debugging gear.
+ *
  * Use the special make targets to see what the assembler looks
  * like:
  *
  * make out/host/obc/tst/unittest-barrier.c
  * make out/host/obc/tst/unittest-barrier.s
  */
-static void example()
+
+#include "com/diag/diminuto/diminuto_unittest.h"
+#include "com/diag/diminuto/diminuto_barrier.h"
+
+/*
+ * The following do_* functions exist so that this
+ * code is more easily found in the resulting assembler
+ * code.
+ */
+
+static volatile int lock = 0;
+
+static int value = 0xdeadbeef;
+
+void do_sync_synchronize()
 {
-        diminuto_barrier();
-        diminuto_acquire();
-        diminuto_release();
+#if defined(COM_DIAG_DIMINUTO_BARRIER)
+    __sync_synchronize();
+#endif
+}
+
+void do_sync_lock_test_and_set()
+{
+#if defined(COM_DIAG_DIMINUTO_ACQUIRE)
+    __sync_lock_test_and_set(&lock, value);
+#endif
+}
+
+void do_sync_lock_release()
+{
+#if defined(COM_DIAG_DIMINUTO_RELEASE)
+    __sync_lock_release(&lock);
+#endif
+}
+
+void do_functions()
+{
+    diminuto_barrier();
+    diminuto_acquire();
+    diminuto_release();
 }
 
 int main(void)
@@ -35,32 +66,51 @@ int main(void)
 
     {
         TEST();
+
 #if !defined(COM_DIAG_DIMINUTO_BARRIER)
         FAILURE();
 #endif
-        STATUS();
-    }
 
-    {
-        TEST();
 #if !defined(COM_DIAG_DIMINUTO_ACQUIRE)
         FAILURE();
 #endif
-        STATUS();
-    }
 
-    {
-        TEST();
 #if !defined(COM_DIAG_DIMINUTO_RELEASE)
         FAILURE();
 #endif
+
         STATUS();
     }
 
     {
         TEST();
-        printf("&example=%p\n", &example);
-        example();
+
+        /*
+         * This is here mostly to keep the compiler from
+         * optimizing out the function implementations.
+         */
+
+        printf("&do_sync_synchronize=%p\n", &do_sync_synchronize);
+        do_sync_synchronize();
+
+        printf("&lock=%p\n", &lock);
+        printf("lock=0x%x=%u=%d\n", lock, lock, lock);
+
+        printf("&do_sync_lock_test_and_set=%p\n", &do_sync_lock_test_and_set);
+        do_sync_lock_test_and_set();
+
+        printf("&lock=%p\n", &lock);
+        printf("lock=0x%x=%u=%d\n", lock, lock, lock);
+
+        printf("&do_sync_lock_release=%p\n", &do_sync_lock_release);
+        do_sync_lock_release();
+
+        printf("&lock=%p\n", &lock);
+        printf("lock=0x%x=%u=%d\n", lock, lock, lock);
+
+        printf("&do_functions=%p\n", &do_functions);
+        do_functions();
+
         STATUS();
     }
 
