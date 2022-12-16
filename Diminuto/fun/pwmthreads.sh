ea7c69bca2e3dfcf2e51c1d220f423a5b656d7f6
Chip Overclock <coverclock@diag.com>
2022-12-16 07:11:22 -0700

    [1] Doubled test cases in pwmthreads, adding 255-0 to 0-255. PASSED

--- Diminuto/fun/pwmthreads.sh
+++ Diminuto/fun/pwmthreads.sh
@@ -7,7 +7,7 @@
 # ABSTRACT
 #
 # Run the pwmthread program on a specified GPIO pin with a duty cycle from
-# 0 to 100.
+# 0% to 100%.
 #
 # USAGE
 #
@@ -20,16 +20,16 @@
 
 . $(readlink -e $(dirname ${0})/../fun)/hardware_test_fixture
 
-. $(readlink -e $(dirname ${0})/../bin)/setup
-
 PGM=$(basename $0)
 PIN=${1:-${HARDWARE_TEST_FIXTURE_PIN_PWM_LED}}
-SEC=${2:-3}
-DUTY=0;
-MAX=255;
-DIR="tmp"
+SEC=${2:-1}
+MIN=0
+MAX=255
+DUTY=${MIN}
 
-mkdir -p ${DIR}
+. $(readlink -e $(dirname ${0})/../bin)/setup
+
+pintool -p ${PIN} -x -o -c -n 2> /dev/null
 
 while [[ ${DUTY} -le ${MAX} ]]; do
 	pwmthread ${PIN} ${DUTY} &
@@ -38,11 +38,21 @@ while [[ ${DUTY} -le ${MAX} ]]; do
 	echo ${PGM}: pin ${PIN} sec ${SEC} dty ${DUTY} pid ${PID} 1>&2
 	sleep ${SEC}
 	kill -15 ${PID}
-	#wait ${PID}
 	wait
 	DUTY=$((${DUTY} + 1))
 done
 
-pintool -p ${PIN} -n 2> /dev/null
+while [[ ${DUTY} -ge ${MIN} ]]; do
+	pwmthread ${PIN} ${DUTY} &
+	PID=$!
+	trap "kill ${PID}; exit" 1 2 3 15
+	echo ${PGM}: pin ${PIN} sec ${SEC} dty ${DUTY} pid ${PID} 1>&2
+	sleep ${SEC}
+	kill -15 ${PID}
+	wait
+	DUTY=$((${DUTY} - 1))
+done
+
+pintool -p ${PIN} -x -o -c -n 2> /dev/null
 
 exit 0
