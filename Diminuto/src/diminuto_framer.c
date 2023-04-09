af09d2590ea02b27d8e044969ddd2dd1a0ec1078
Chip Overclock <coverclock@diag.com>
2023-04-09 10:41:31 -0600

    [1] Minor tweak to "complete" log output.

--- Diminuto/src/diminuto_framer.c
+++ Diminuto/src/diminuto_framer.c
@@ -677,8 +677,8 @@ ssize_t diminuto_framer_reader(FILE * stream, diminuto_framer_t * that)
                 DIMINUTO_LOG_INFORMATION("diminuto_framer@%p: empty?\n", that);
                 diminuto_framer_reset(that);
             } else {
+                DIMINUTO_LOG_DEBUG("diminuto_framer@%p: complete. [%zd] [%zu]\n", that, that->total, that->length);
                 result = that->total;
-                DIMINUTO_LOG_DEBUG("diminuto_framer@%p: complete. [%zd]\n", that, result);
             }
             break;
 
