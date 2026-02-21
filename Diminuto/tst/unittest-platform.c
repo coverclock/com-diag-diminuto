3153c68a5556bec130c722c32769f451089014e5
Chip Overclock <coverclock@diag.com>
2026-02-21 12:25:55 -0700

    [1] Improved Platform unit test. PASSED [2] Bumped BUILD release number. BUILDS

--- Diminuto/tst/unittest-platform.c
+++ Diminuto/tst/unittest-platform.c
@@ -10,6 +10,7 @@
  * This is a unit test of the plaform info.
  */
 
+#include <string.h>
 #include "com/diag/diminuto/diminuto_unittest.h"
 #include "com/diag/diminuto/diminuto_platform.h"
 
@@ -29,22 +30,72 @@ int main(void)
 	}
 
     {
-        struct utsname buffer;
+        static const size_t LIMIT = 65;
+        struct utsname buffer = { 0, };
         int rc;
+        size_t ss;
 
         TEST();
 
         rc = diminuto_platform_system(&buffer);
         ASSERT(rc >= 0);
 
-        printf("COM_DIAG_DIMINUTO_PLATFORM=\"%s\"\n", COM_DIAG_DIMINUTO_PLATFORM);
-        printf("sysname[%zu]=\"%s\"\n", sizeof(buffer.sysname), buffer.sysname);
-        printf("nodename[%zu]=\"%s\"\n", sizeof(buffer.nodename), buffer.nodename);
-        printf("release[%zu]=\"%s\"\n", sizeof(buffer.release), buffer.release);
-        printf("version[%zu]=\"%s\"\n", sizeof(buffer.version), buffer.version);
-        printf("machine[%zu]=\"%s\"\n", sizeof(buffer.machine), buffer.machine);
+        {
+            ASSERT(COM_DIAG_DIMINUTO_PLATFORM != (char *)0);
+            ASSERT(sizeof(COM_DIAG_DIMINUTO_PLATFORM) > 0);
+            ASSERT(COM_DIAG_DIMINUTO_PLATFORM[0] != '\0');
+            ASSERT((ss = strlen(COM_DIAG_DIMINUTO_PLATFORM)) < LIMIT);
+            printf("COM_DIAG_DIMINUTO_PLATFORM[%zu][%zu]=\"%s\"\n", sizeof(COM_DIAG_DIMINUTO_PLATFORM), ss,  COM_DIAG_DIMINUTO_PLATFORM);
+        }
+
+        {
+            ASSERT(buffer.sysname != (char *)0);
+            ASSERT(sizeof(buffer.sysname) > 0);
+            ASSERT(buffer.sysname[0] != '\0');
+            ASSERT((ss = strlen(buffer.sysname)) < LIMIT);
+            printf("sysname[%zu][%zu]=\"%s\"\n", sizeof(buffer.sysname), ss, buffer.sysname);
+        }
+
+        {
+            ASSERT(buffer.nodename != (char *)0);
+            ASSERT(sizeof(buffer.nodename) > 0);
+            ASSERT(buffer.nodename[0] != '\0');
+            ASSERT((ss = strlen(buffer.nodename)) < LIMIT);
+            printf("nodename[%zu][%zu]=\"%s\"\n", sizeof(buffer.nodename), ss, buffer.nodename);
+        }
+
+        {
+            ASSERT(buffer.release != (char *)0);
+            ASSERT(sizeof(buffer.release) > 0);
+            ASSERT(buffer.release[0] != '\0');
+            ASSERT((ss = strlen(buffer.release)) < LIMIT);
+            printf("release[%zu][%zu]=\"%s\"\n", sizeof(buffer.release), ss, buffer.release);
+        }
+
+        {
+            ASSERT(buffer.version != (char *)0);
+            ASSERT(sizeof(buffer.version) > 0);
+            ASSERT(buffer.version[0] != '\0');
+            ASSERT((ss = strlen(buffer.version)) < LIMIT);
+            printf("version[%zu][%zu]=\"%s\"\n", sizeof(buffer.version), ss, buffer.version);
+        }
+
+        {
+            ASSERT(buffer.machine != (char *)0);
+            ASSERT(sizeof(buffer.machine) > 0);
+            ASSERT(buffer.machine[0] != '\0');
+            ASSERT((ss = strlen(buffer.machine)) < LIMIT);
+            printf("machine[%zu][%zu]=\"%s\"\n", sizeof(buffer.machine), ss, buffer.machine);
+        }
+
 #ifdef _GNU_SOURCE
-        printf("domainname[%zu]=\"%s\"\n", sizeof(buffer.domainname), buffer.domainname);
+        {
+            ASSERT(buffer.domainname != (char *)0);
+            ASSERT(sizeof(buffer.domainname) > 0);
+            ASSERT(buffer.domainname[0] != '\0');
+            ASSERT((ss = strlen(buffer.domainname)) < LIMIT);
+            printf("domainname[%zu][%zu]=\"%s\"\n", sizeof(buffer.domainname), ss, buffer.domainname);
+        }
 #endif
 
         STATUS();
